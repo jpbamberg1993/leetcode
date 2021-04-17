72a56a04437c936fc97d2a56bf6a42386b7d6f7c
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-04-17 17:24:18 -0400

    Longest Substring Without Repeating Characters: Ensure it passes all Leetcode tests and add test that failed

--- 3-longest-substring-without-repeating-characters/ts-version/src/length-of-longest-substring.ts
+++ 3-longest-substring-without-repeating-characters/ts-version/src/length-of-longest-substring.ts
@@ -1,2 +1,16 @@
 export function lengthOfLongestSubstring(s: string): number {
+  const characters = Array.from(s)
+  let maxLength = 0
+  let currentArr = []
+  let startingIndex = 0
+  for (let i = 0; i < characters.length; i++) {
+    if (currentArr.includes(characters[i])) {
+      while (startingIndex < i && currentArr.shift() != characters[i]) {
+        startingIndex++
+      }
+    }
+    currentArr.push(characters[i])
+    maxLength = currentArr.length > maxLength ? currentArr.length : maxLength
+  }
+  return maxLength === 0 ? currentArr.length : maxLength
 }
--- 3-longest-substring-without-repeating-characters/ts-version/tests/length-of-longest-substring.test.ts
+++ 3-longest-substring-without-repeating-characters/ts-version/tests/length-of-longest-substring.test.ts
@@ -9,7 +9,7 @@ describe('lengthOfLongestSubstring', function() {
     expect(result).toBe(3)
   })
 
-  it('returns longest substring', function() {
+  it('returns longest substring where all characters repeat', function() {
     const s = 'bbbbb'
 
     const result = lengthOfLongestSubstring(s)
@@ -17,7 +17,7 @@ describe('lengthOfLongestSubstring', function() {
     expect(result).toBe(1)
   })
 
-  it('returns longest substring', function() {
+  it('returns longest substring with repeating characters', function() {
     const s = 'pwwkew'
 
     const result = lengthOfLongestSubstring(s)
@@ -25,11 +25,27 @@ describe('lengthOfLongestSubstring', function() {
     expect(result).toBe(3)
   })
 
-  it('returns longest substring', function() {
+  it('returns longest substring with an empty string', function() {
     const s = ''
 
     const result = lengthOfLongestSubstring(s)
 
     expect(result).toBe(0)
+  })
+
+  it('returns longest substring with only a blank space for a string', function() {
+    const s = ' '
+
+    const result = lengthOfLongestSubstring(s)
+
+    expect(result).toBe(1)
+  })
+
+  it('returns longest substring from aab', function() {
+    const s = 'aab'
+
+    const result = lengthOfLongestSubstring(s)
+
+    expect(result).toBe(2)
   })
 })
