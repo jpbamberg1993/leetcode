0f5474458e8d800401b54c45bc59cc6acbe3ca7a
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-04-17 11:36:27 -0400

    Un comment tests

--- 2-add-two-numbers/ts-version/src/add-two-numbers.ts
+++ 2-add-two-numbers/ts-version/src/add-two-numbers.ts
@@ -1,25 +1,30 @@
 import { ListNode } from "./ListNode"
 import { LinkedList } from './LinkedList';
 
+// For the leetcode solution change LinkedList to LinkedNode and return the first node
+// It is too difficult to test passing the first LinkedNode here
 export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): LinkedList | null {
   let sum = new LinkedList()
-  let carry: number = 0
+  let carry = 0
+
   while (l1 && l2) {
     const result: number = l1.val + l2.val
-    const added = carry + (result % 10)
+    const added: number = carry + (result % 10)
     sum.addLast(added)
     carry = result >= 10 ? 1 : 0
     l1 = l1.next
     l2 = l2.next
   }
+
   let notNullNode: ListNode = l1 || l2
   while (notNullNode) {
-    const result = carry + notNullNode.val
-    const added = result % 10
+    const result: number = carry + notNullNode.val
+    const added: number = result % 10
     sum.addLast(added)
     carry = result >= 10 ? 1 : 0
     notNullNode = notNullNode.next
   }
+
   if (carry === 1) sum.addLast(1)
   return sum
 }
--- 2-add-two-numbers/ts-version/tests/add-two-numbers.test.ts
+++ 2-add-two-numbers/ts-version/tests/add-two-numbers.test.ts
@@ -3,25 +3,25 @@ import { LinkedList } from "../src/LinkedList";
 import { ListNode } from "../src/ListNode";
 
 describe('addTwoNumbers', function() {
-  // it('add two numbers as linked lists', function() {
-  //   const linkedList1 = generateLinkedList([2,4,3])
-  //   const linkedList2 = generateLinkedList([5,6,4])
-  //   const expectedResult = generateLinkedList([7,0,8])
+  it('add two numbers as linked lists', function() {
+    const linkedList1 = generateLinkedList([2,4,3])
+    const linkedList2 = generateLinkedList([5,6,4])
+    const expectedResult = generateLinkedList([7,0,8])
 
-  //   const result = addTwoNumbers(linkedList1.first, linkedList2.first)
+    const result = addTwoNumbers(linkedList1.first, linkedList2.first)
 
-  //   expect(result).toStrictEqual(expectedResult)
-  // })
+    expect(result).toStrictEqual(expectedResult)
+  })
 
-  // it('adds linked lists with only 1 value each', function() {
-  //   const linkedList1 = generateLinkedList([0])
-  //   const linkedList2 = generateLinkedList([0])
-  //   const expectedResult = generateLinkedList([0])
+  it('adds linked lists with only 1 value each', function() {
+    const linkedList1 = generateLinkedList([0])
+    const linkedList2 = generateLinkedList([0])
+    const expectedResult = generateLinkedList([0])
 
-  //   const result = addTwoNumbers(linkedList1.first, linkedList2.first)
+    const result = addTwoNumbers(linkedList1.first, linkedList2.first)
 
-  //   expect(result).toStrictEqual(expectedResult)
-  // })
+    expect(result).toStrictEqual(expectedResult)
+  })
 
   it('adds differently sized linked lists', function() {
     const linkedList1 = generateLinkedList([9,9,9,9,9,9,9])
