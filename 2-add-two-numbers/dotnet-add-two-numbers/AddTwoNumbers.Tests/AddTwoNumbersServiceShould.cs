1f13088fa1cf74b62f248ccfb8c3e87e2117795a
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-11-11 10:31:54 -0500

    Set up basic tests for Add Two Numbers challenge

--- 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers.Tests/AddTwoNumbersServiceShould.cs
+++ 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers.Tests/AddTwoNumbersServiceShould.cs
@@ -1,7 +1,84 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
 namespace AddTwoNumbers.Tests
 {
     public class AddTwoNumbersServiceShould
     {
-        
+        private AddTwoNumbersService _addTwoNumbersService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _addTwoNumbersService = new AddTwoNumbersService();
+        }
+
+        [Test]
+        public void AddTwoNumbers_InputTwoArraysOfDiffDigits_ReturnCorrectSumInReverse()
+        {
+            var listOneArr = new[] {2, 4, 3};
+            var listTwoArr = new[] {5, 6, 4};
+
+            var listOne = ArrayToLinkedList(listOneArr);
+            var listTwo = ArrayToLinkedList(listTwoArr);
+
+            var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
+
+            var expectedAnswerArr = new[] {7, 0, 8};
+            var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
+
+            Assert.True(result.Equals(expectedAnswer));
+        }
+
+        [Test]
+        public void AddTwoNumbers_InputTwoArraysOfZero_ReturnZero()
+        {
+            var listOneArr = new[] {0};
+            var listTwoArr = new[] {0};
+
+            var listOne = ArrayToLinkedList(listOneArr);
+            var listTwo = ArrayToLinkedList(listTwoArr);
+
+            var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
+
+            var expectedAnswerArr = new[] {0};
+            var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
+
+            Assert.True(result.Equals(expectedAnswer));
+        }
+
+        [Test]
+        public void AddNumbers_InputTwoArraysWithCarryOver_ReturnCorrectList()
+        {
+            var listOneArr = new[] {9, 9, 9, 9, 9, 9, 9};
+            var listTwoArr = new[] {9, 9, 9, 9};
+
+            var listOne = ArrayToLinkedList(listOneArr);
+            var listTwo = ArrayToLinkedList(listTwoArr);
+
+            var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
+
+            var expectedAnswerArr = new[] {8, 9, 9, 9, 0, 0, 0, 1};
+            var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
+
+            Assert.True(result.Equals(expectedAnswer));
+        }
+
+        private ListNode ArrayToLinkedList(int[] listOneArr)
+        {
+            var firstNode = new ListNode(listOneArr[0]);
+            var currentNode = firstNode;
+
+            for (var index = 1; index < listOneArr.Length; index++)
+            {
+                var item = listOneArr[index];
+                var node = new ListNode(item);
+
+                currentNode.next = node;
+                currentNode = node;
+            }
+
+            return firstNode;
+        }
     }
 }
--- 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers/AddTwoNumbersService.cs
+++ 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers/AddTwoNumbersService.cs
@@ -2,6 +2,9 @@ namespace AddTwoNumbers
 {
     public class AddTwoNumbersService
     {
-        
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return new ListNode();
+        }
     }
 }
