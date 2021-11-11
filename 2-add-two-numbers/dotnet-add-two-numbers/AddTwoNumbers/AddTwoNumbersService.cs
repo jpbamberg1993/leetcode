18afcd6c66e7b0a40d2efc9eb31652de151f69dd
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-11-11 13:15:00 -0500

    Get initial solution to pass tests

--- 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers/AddTwoNumbersService.cs
+++ 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers/AddTwoNumbersService.cs
@@ -2,9 +2,43 @@ namespace AddTwoNumbers
 {
     public class AddTwoNumbersService
     {
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        public ListNode? AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            return new ListNode();
+            ListNode head = null;
+            ListNode current = null;
+
+            var remainder = 0;
+            while (l1 != null || l2 != null)
+            {
+                var sum = (l1?.val ?? 0) + (l2?.val ?? 0) + remainder;
+                remainder = sum >= 10 ? 1 : 0;
+                sum %= 10;
+                var node = new ListNode
+                {
+                    val = sum
+                };
+
+                if (head == null || current == null)
+                {
+                    head = node;
+                    current = node;
+                }
+                else
+                {
+                    current.next = node;
+                    current = current.next;
+                }
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            if (remainder > 0)
+            {
+                current.next = new ListNode(remainder);
+            }
+
+            return head;
         }
     }
 }
