d512e40b56331885597be08ce8162bbfdde16b05
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-11-13 08:47:13 -0500

    Get initial tests passing

--- 3-longest-substring-without-repeating-characters/longest-substring-dotnet/LongestSubstring/LongestSubstringService.cs
+++ 3-longest-substring-without-repeating-characters/longest-substring-dotnet/LongestSubstring/LongestSubstringService.cs
@@ -1,10 +1,35 @@
+using System;
+
 namespace LongestSubstring
 {
-    public class LongestSubstringService
+    public static class LongestSubstringService
     {
         public static int LengthOfLongestSubstring(string s)
         {
-            return 0;
+            var longestSubString = 0;
+            var startIndex = 0;
+            var endIndex = 0;
+
+            while (endIndex < s.Length)
+            {
+                if (startIndex == endIndex)
+                {
+                    endIndex += 1;
+                    continue;
+                }
+
+                var indexOfPreviousChar = s.IndexOf(s[endIndex], startIndex, endIndex - startIndex);
+                if (indexOfPreviousChar != -1)
+                {
+                    var diff = endIndex - startIndex;
+                    longestSubString = Math.Max(diff, longestSubString);
+                    startIndex = indexOfPreviousChar  + 1;
+                }
+
+                endIndex += 1;
+            }
+
+            return longestSubString;
         }
     }
 }
