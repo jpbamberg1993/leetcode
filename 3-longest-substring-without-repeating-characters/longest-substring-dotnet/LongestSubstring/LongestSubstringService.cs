86bdece9e3006a9a88388b3006a6b9d7e7e2b7e9
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-11-14 12:03:45 -0500

    Passing char as index will automatically convert to character value

--- 3-longest-substring-without-repeating-characters/longest-substring-dotnet/LongestSubstring/LongestSubstringService.cs
+++ 3-longest-substring-without-repeating-characters/longest-substring-dotnet/LongestSubstring/LongestSubstringService.cs
@@ -7,7 +7,6 @@ namespace LongestSubstring
         public static int LengthOfLongestSubstring(string s)
         {
             var chars = new int[128];
-            var sArr = s.ToCharArray();
             
             var left = 0;
             var right = 0;
@@ -15,11 +14,12 @@ namespace LongestSubstring
             var res = 0;
             while (right < s.Length)
             {
-                chars[sArr[right]] += 1;
+                var r = s[right];
+                chars[r] += 1;
 
-                while (chars[sArr[right]] > 1)
+                while (chars[r] > 1)
                 {
-                    var l = sArr[left];
+                    var l = s[left];
                     chars[l]--;
                     left++;
                 }
