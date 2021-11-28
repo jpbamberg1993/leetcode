269a77b139cc4c0ed758bb40d472922f6f557c63
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-11-28 13:17:22 -0500

    Set up initial project for finding the median of two sorted arrays

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays-dotnet/MedianOfTwoSortedArrays.Tests/MedianArrayServiceShould.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace MedianOfTwoSortedArrays.Tests;
+
+public class MedianArrayServiceShould
+{
+    private MedianArrayService _medianArrayService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _medianArrayService = new MedianArrayService();
+    }
+
+    [Test]
+    public void FindMedianSortedArrays_InputThreeNumbers_ReturnTwo()
+    {
+        var nums1 = new[] {1, 3};
+        var nums2 = new[] {2};
+
+        var median = _medianArrayService.FindMedianSortedArrays(nums1, nums2);
+
+        const double expectedMedian = 2.0000;
+        
+        Assert.AreEqual(expectedMedian, median);
+    }
+
+    [Test]
+    public void FindMedianSortedArrays_InputEvenNumbers_ReturnTwoPointFive()
+    {
+        var nums1 = new[] {1, 2};
+        var nums2 = new[] {3,4};
+
+        var median = _medianArrayService.FindMedianSortedArrays(nums1, nums2);
+
+        const double expectedMedian = 2.5000;
+
+        Assert.AreEqual(expectedMedian, median);
+    }
+}
---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays-dotnet/MedianOfTwoSortedArrays/MedianArrayService.cs
@@ -0,0 +1,9 @@
+﻿namespace MedianOfTwoSortedArrays;
+
+public class MedianArrayService
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        return 0.00;
+    }
+}
