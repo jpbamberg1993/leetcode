81c05dcb6badf07155097640c9a7a9e0d502b2de
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-12-02 08:20:26 -0500

    Write function to determine the median of two sorted arrays

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays-dotnet/MedianOfTwoSortedArrays/MedianArrayService.cs
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays-dotnet/MedianOfTwoSortedArrays/MedianArrayService.cs
@@ -4,6 +4,24 @@ public class MedianArrayService
 {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2)
     {
-        return 0.00;
+        var nums = nums1.Concat(nums2).ToArray();
+        Array.Sort(nums);
+
+        var numsLength = nums.Length;
+        var lengthIsEven = numsLength % 2 == 0;
+
+        if (lengthIsEven)
+        {
+            var firstIndex = numsLength / 2 - 1;
+            var firstNum = nums[firstIndex];
+            var secondNum = nums[firstIndex + 1];
+            
+            var result = (firstNum + secondNum) / 2.0;
+            return result;
+        }
+
+        var middleIndex = (int)Math.Ceiling(numsLength / 2.0) - 1;
+
+        return nums[middleIndex];
     }
 }
