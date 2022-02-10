69c3101a5e6a558df3f3ed4fc8dffe0277ad6599
J. Paul Bamberg <jpbamberg1993@gmail.com>
2022-02-09 22:50:53 -0500

    Make test pass when longest substring is of length 1 or 2

--- 5-longest-palindromic-substring/longest-palindromic-substring/src/longest-palindrome.ts
+++ 5-longest-palindromic-substring/longest-palindromic-substring/src/longest-palindrome.ts
@@ -1,3 +1,30 @@
 export function longestPalindrome(s: string): string {
-  return "";
+    const sLength = s.length
+
+    if (sLength <= 1) {
+        return s
+    }
+
+    const palindromeDictionary: boolean[][] = []
+    let longestSubStringLength = 1
+    let longestSubStringStartingIndex = 0
+
+    for (let i = 0; i < sLength; i++) {
+        palindromeDictionary[i] = []
+        palindromeDictionary[i][i] = true
+    }
+
+    for (let i = 0; i < sLength - 1; i++) {
+        if (s.charAt(i) === s.charAt(i + 1)) {
+            palindromeDictionary[i][i + 1] = true
+            longestSubStringStartingIndex = i
+            longestSubStringLength = 2
+        } else {
+            palindromeDictionary[i][i + 1] = true
+        }
+    }
+
+    return s.substring(
+        longestSubStringStartingIndex,
+        longestSubStringStartingIndex + longestSubStringLength)
 }
--- 5-longest-palindromic-substring/longest-palindromic-substring/tests/longest-palindrome.test.ts
+++ 5-longest-palindromic-substring/longest-palindromic-substring/tests/longest-palindrome.test.ts
@@ -1,9 +1,15 @@
-import { longestPalindrome } from '../src/longest-palindrome'
+import {longestPalindrome} from '../src/longest-palindrome'
 
-describe('longestPalindrome', function() {
-  it('is passed babad and returns bab', function() {
-    const result = longestPalindrome('babad')
+describe('longestPalindrome', function () {
+    it('is passed "babad" and returns "bab"', function () {
+        const result = longestPalindrome('babad')
 
-    expect(result).toEqual('bab')
-  })
+        expect(result).toEqual('bab')
+    })
+
+    it('is passed "cbbd" and returns "cbbd"', function () {
+        const result = longestPalindrome("cbbd")
+
+        expect(result).toEqual('bb')
+    })
 })
