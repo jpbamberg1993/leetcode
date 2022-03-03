d47f377dcd8c910fc5ed76d9b7b402b6dbb4b2bc
J. Paul Bamberg <jpbamberg1993@gmail.com>
2022-03-03 12:13:35 -0500

    Create algorithm that passes initial zigzag tests

--- 6-zigzag-conversion/ZigzagConversion/ZigzagConversion/ZigzagConversion.cs
+++ 6-zigzag-conversion/ZigzagConversion/ZigzagConversion/ZigzagConversion.cs
@@ -4,6 +4,47 @@ public static class ZigzagConversion
 {
     public static string Convert(string s, int numRows)
     {
+        if (s.Length <= 1)
+        {
+            return s;
+        }
         
+        var zigzagDict = new List<char>[numRows].Select(c => new List<char>()).ToArray();
+
+        for (var i = 0; i < numRows; i++)
+        {
+            zigzagDict[i].Add(s[i]);
+        }
+
+        var goingDown = true;
+        var currentRow = numRows - 1;
+
+        for (var i = numRows; i < s.Length; i++)
+        {
+            if (currentRow == 0 || (currentRow + 1) % numRows == 0)
+            {
+                goingDown = !goingDown;
+            }
+
+            if (goingDown)
+            {
+                currentRow++;
+            }
+            else
+            {
+                currentRow--;
+            }
+            
+            zigzagDict[currentRow].Add(s[i]);
+        }
+
+        var dictString = "";
+
+        foreach (var listOfChars in zigzagDict)
+        {
+            dictString += string.Join("", listOfChars);
+        }
+
+        return dictString;
     }
 }
