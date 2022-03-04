db276c67d26cf32fc0591e04f2a51c870f65346c
J. Paul Bamberg <jpbamberg1993@gmail.com>
2022-03-04 18:40:41 -0500

    Zigzag conversion solve in Typescript

--- 6-zigzag-conversion/zigzag-conversion/src/zigzag-conversion.ts
+++ 6-zigzag-conversion/zigzag-conversion/src/zigzag-conversion.ts
@@ -1,3 +1,25 @@
 export function zigzagConversion(s: string, numRows: number): string {
+  if (numRows === 1) {
+    return s
+  }
 
+  let goingDown: boolean = false
+  let currentRow: number = 0
+  let zigzagDict: string[] = new Array(numRows)
+
+  for (let i = 0; i < numRows; i++) {
+    zigzagDict[i] = ""
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    zigzagDict[currentRow] += s.charAt(i)
+
+    if (currentRow === 0 || currentRow === numRows - 1) {
+      goingDown = !goingDown
+    }
+
+    currentRow += goingDown ? 1 : -1
+  }
+
+  return zigzagDict.join("")
 }
--- 6-zigzag-conversion/zigzag-conversion/tests/zigzag-conversion.test.ts
+++ 6-zigzag-conversion/zigzag-conversion/tests/zigzag-conversion.test.ts
@@ -1,4 +1,30 @@
+import {zigzagConversion} from '../src/zigzag-conversion'
+
 describe('zigzagConversion', function() {
-  it('is passed ("PAYPALISHIRING", 3) and returns "PAHNAPLSIIGYIR"', function() {
+  it('is passed ("PAYPALISHIRING", 3) and returns "PAHNAPLSIIGYIR"', function () {
+    const s: string = "PAYPALISHIRING"
+    const numRows: number = 3
+
+    const result: string = zigzagConversion(s, numRows)
+
+    expect(result).toEqual("PAHNAPLSIIGYIR")
+  })
+
+  it('is passed ("PAYPALISHIRING", 4) and returns "PINALSIGYAHRPI"', function () {
+    const s: string = "PAYPALISHIRING"
+    const numRows: number = 4
+
+    const result: string = zigzagConversion(s, numRows)
+
+    expect(result).toEqual("PINALSIGYAHRPI")
+  })
+
+  it('is passed ("A", 1) and returns "A"', function () {
+    const s: string = "A"
+    const numRows: number = 1
+
+    const result: string = zigzagConversion(s, numRows)
+
+    expect(result).toEqual("A")
   })
 })
