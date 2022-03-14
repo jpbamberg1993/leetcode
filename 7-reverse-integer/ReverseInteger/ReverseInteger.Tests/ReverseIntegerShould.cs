7b76999830d78120b99aea0906369f34452df818
J. Paul Bamberg <jpbamberg1993@gmail.com>
2022-03-14 09:37:58 -0400

    Set up initial tests

---
+++ 7-reverse-integer/ReverseInteger/ReverseInteger.Tests/ReverseIntegerShould.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+
+namespace ReverseInteger.Tests;
+
+public class ReverseIntegerShould
+{
+    private ReverseInteger _reverseInteger = new();
+    
+    [SetUp]
+    public void Setup()
+    {
+        _reverseInteger = new ReverseInteger();
+    }
+
+    [Test]
+    public void Reverse_Input_123_Return_321()
+    {
+        var result = _reverseInteger.Reverse(123);
+        Assert.AreEqual(321, result);
+    }
+    
+    [Test]
+    public void Reverse_Input_Neg123_Return_Neg321()
+    {
+        var result = _reverseInteger.Reverse(-123);
+        Assert.AreEqual(-321, result);
+    }
+    
+    [Test]
+    public void Reverse_Input_120_Return_21()
+    {
+        var result = _reverseInteger.Reverse(120);
+        Assert.AreEqual(21, result);
+    }
+}
