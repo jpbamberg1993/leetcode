ddadb7819b228fbf9972227a287660c50fcb2fd9
J. Paul Bamberg <jpbamberg1993@gmail.com>
2022-04-12 20:28:19 -0400

    Get initial tests to pass for converting a string to an integer

--- 8-string-to-integer/StringToInteger/StringToInteger/ConvertString.cs
+++ 8-string-to-integer/StringToInteger/StringToInteger/ConvertString.cs
@@ -1,9 +1,31 @@
-﻿namespace StringToInteger;
+﻿using System.Text.RegularExpressions;
+
+namespace StringToInteger;
 
 public static class ConvertString
 {
     public static int MyAtoi(string s)
     {
-        return 0;
+        var trimmedString = s.Trim();
+        var returnString = string.Empty;
+        var isNegative = trimmedString[0] == '-';
+
+        foreach (var d in trimmedString)
+        {
+            if (d is '-' or '+') continue;
+
+            if (!char.IsDigit(d)) break;
+
+            returnString += d;
+        }
+
+        var tryParse = int.TryParse(returnString, out var returnValue);
+
+        if (!tryParse)
+        {
+            throw new Exception($"unable to parse return string {returnString}");
+        }
+
+        return isNegative ? returnValue * -1 : returnValue;
     }
 }
