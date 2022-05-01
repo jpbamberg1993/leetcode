a68a6741dd3cdeb7da4c6074d16dabe168fcbd1f
J. Paul Bamberg <jpbamberg1993@gmail.com>
2022-05-01 16:33:07 -0400

    TS String to integer: implement initial solution

--- 8-string-to-integer/string-to-integer/src/string-to-integer.ts
+++ 8-string-to-integer/string-to-integer/src/string-to-integer.ts
@@ -1,3 +1,36 @@
 export function myAtoi(s: string): number {
-  return 0
+  let currentIndex: number = 0
+  let sign: number = 1
+  let result: number = 0
+
+  while (currentIndex < s.length && s.charAt(currentIndex) === ' ') {
+    currentIndex++
+  }
+
+  if (currentIndex < s.length && s.charAt(currentIndex) === '-') {
+    sign = -1
+    currentIndex++
+  } else if (currentIndex < s.length && s.charAt(currentIndex) === '+') {
+    currentIndex++
+  }
+
+  while (currentIndex < s.length && charIsDigit(s.charAt(currentIndex))) {
+    const currentNumber: number = parseInt(s.charAt(currentIndex))
+
+    if (
+      result > Number.MAX_SAFE_INTEGER / 10 ||
+      (result === Number.MAX_SAFE_INTEGER && currentNumber > Number.MAX_SAFE_INTEGER % 10)) {
+      result = Number.MAX_SAFE_INTEGER
+      break
+    }
+
+    result = result * 10 + currentNumber
+    currentIndex++
+  }
+
+  return result * sign
+}
+
+function charIsDigit(s: string): boolean {
+  return s >= '0' && s <= '9'
 }
