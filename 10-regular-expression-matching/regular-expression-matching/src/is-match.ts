283c6fb548f4180ad69f550df513c23c9872d1e2
J. Paul Bamberg <jpbamberg1993@gmail.com>
2022-07-03 15:50:53 -0400

    TS - Expression Matching: Solve using dynamic programming

--- 10-regular-expression-matching/regular-expression-matching/src/is-match.ts
+++ 10-regular-expression-matching/regular-expression-matching/src/is-match.ts
@@ -1,11 +1,39 @@
-export function isMatch(s: string, p: string): boolean {
-  if (p === '') return s === ''
+export class ExpressionMatching {
+  private memo: Result[][];
 
-  const firstMatch: boolean = s !== '' && (p.charAt(0) === s.charAt(0) || p.charAt(0) === '.')
-
-  if (p.length >= 2 && p.charAt(1) === '*') {
-    return (isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p)))
+  public isMatch(s: string, p: string): boolean {
+    this.memo = Array(s.length + 1)
+      .fill(null)
+      .map(() => Array(p.length + 1))
+    return this.isMatchExpression(0, 0, s, p)
   }
 
-  return firstMatch && isMatch(s.substring(1), p.substring(1))
+  private isMatchExpression(i: number, j: number, s: string, p: string): boolean {
+    if (this.memo[i][j] !== undefined) {
+      return this.memo[i][j] === Result.TRUE;
+    }
+
+    let answer: boolean = false
+    if (j === p.length) {
+      answer = i === s.length
+    } else {
+      const firstMatch = i < s.length && (p[j] === s[i] || p[j] === '.')
+
+      if (j + 1 < p.length && p[j + 1] === '*') {
+        answer = this.isMatchExpression(i, j + 2, s, p) ||
+          (firstMatch && this.isMatchExpression(i + 1, j, s, p))
+      } else {
+        answer = firstMatch && this.isMatchExpression(i + 1, j + 1, s, p)
+      }
+    }
+
+    this.memo[i][j] = answer ? Result.TRUE : Result.FALSE
+    return answer
+  }
+}
+
+enum Result {
+  UNSET,
+  TRUE,
+  FALSE
 }
--- 10-regular-expression-matching/regular-expression-matching/tests/is-match.test.ts
+++ 10-regular-expression-matching/regular-expression-matching/tests/is-match.test.ts
@@ -1,23 +1,27 @@
-import {isMatch} from '../src/is-match'
+import {ExpressionMatching} from '../src/is-match'
 
 describe('isMatch', function() {
+  let expressionMatching: ExpressionMatching;
+
+  beforeEach(() => expressionMatching = new ExpressionMatching)
+
   it('is passed "aa" and "a" and returns false', function() {
-    const result = isMatch('aa', 'a')
+    const result = expressionMatching.isMatch('aa', 'a')
     expect(result).toBe(false)
   })
 
   it('is passed "aa" and "a*" and returns true', function() {
-    const result = isMatch('aa', 'a*')
+    const result = expressionMatching.isMatch('aa', 'a*')
     expect(result).toBe(true)
   })
 
   it('is passed "ab" and ".*" and returns true', function() {
-    const result = isMatch("ab", ".*")
+    const result = expressionMatching.isMatch("ab", ".*")
     expect(result).toBe(true)
   })
 
   it('is passed "ab" and ".*c" and returns false', function() {
-    const result = isMatch("ab", ".*c")
+    const result = expressionMatching.isMatch("ab", ".*c")
     expect(result).toBe(false)
   })
 })
