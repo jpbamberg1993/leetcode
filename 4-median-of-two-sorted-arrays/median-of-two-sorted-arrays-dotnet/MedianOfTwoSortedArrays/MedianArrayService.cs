e4d701b8a2f4d4e04982f1efb48fa356c84b67d7
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-09-01 06:45:33 -0500

    Dotnet remove Median Array solution

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays-dotnet/MedianOfTwoSortedArrays/MedianArrayService.cs
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays-dotnet/MedianOfTwoSortedArrays/MedianArrayService.cs
@@ -1,48 +1,17 @@
-﻿namespace MedianOfTwoSortedArrays;
+namespace MedianOfTwoSortedArrays;
 
 public class MedianArrayService
 {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2)
     {
-        var a = nums1;
-        var b = nums2;
-        var length = a.Length + b.Length;
-        var half = length / 2;
+        var leftPointer = 0;
+        var rightPointer = 0;
 
-        if (b.Length < a.Length)
-        {
-            (a, b) = (b, a);
-        }
-
-        var l = 0;
-        var r = a.Length - 1;
         while (true)
         {
-            var i = (int) Math.Floor((r + l) / 2.0);
-            var j = half - i - 2;
-
-            var aLeft = i >= 0 ? a[i] : int.MinValue;
-            var aRight = i + 1 < a.Length ? a[i + 1] : int.MaxValue;
-            var bLeft = j >= 0 ? b[j] : int.MinValue;
-            var bRight = j + 1 < b.Length ?b[j + 1] : int.MaxValue;
-
-            if (aLeft <= bRight && bLeft <= aRight)
+            if (nums1[leftPointer] < nums2[rightPointer])
             {
-                if (length % 2 == 0)
-                {
-                    return (Math.Max(aLeft, bLeft) + Math.Min(aRight, bRight)) / 2.0;
-                }
-
-                return Math.Min(aRight, bRight);
-            }
-
-            if (aLeft > bRight)
-            {
-                r = i - 1;
-            }
-            else
-            {
-                l = i + 1;
+                
             }
         }
     }
