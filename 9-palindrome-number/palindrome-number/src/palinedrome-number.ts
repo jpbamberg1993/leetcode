f7c4fa8580a138eb7fa121c776fe5a7fcb1ce5fe
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-09-24 14:03:56 -0500

    TS - Is Palindrome - Solve again

---
+++ 9-palindrome-number/palindrome-number/src/palinedrome-number.ts
@@ -0,0 +1,20 @@
+export function isPalindrome(x: number): boolean {
+  if (x < 0) return false;
+
+  const original = x;
+  const reversed = x.toString().split("").reverse().join("");
+
+
+  return original.toString() === reversed;
+
+  // const original = x;
+  // let reversed = 0;
+
+  // while (x != 0) {
+  //   const pop = x % 10;
+  //   reversed = reversed * 10 + pop;
+  //   x = Math.floor(x / 10);
+  // }
+
+  // return reversed === original;
+}
