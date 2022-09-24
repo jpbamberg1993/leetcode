9a4d0ee9d69f7f734cdc979f269bbf2078397cfe
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-09-24 14:04:51 -0500

    TS - Palindrome Number - Use solution that does not convert int to string

--- 9-palindrome-number/palindrome-number/src/palinedrome-number.ts
+++ 9-palindrome-number/palindrome-number/src/palinedrome-number.ts
@@ -2,19 +2,13 @@ export function isPalindrome(x: number): boolean {
   if (x < 0) return false;
 
   const original = x;
-  const reversed = x.toString().split("").reverse().join("");
+  let reversed = 0;
 
+  while (x != 0) {
+    const pop = x % 10;
+    reversed = reversed * 10 + pop;
+    x = Math.floor(x / 10);
+  }
 
-  return original.toString() === reversed;
-
-  // const original = x;
-  // let reversed = 0;
-
-  // while (x != 0) {
-  //   const pop = x % 10;
-  //   reversed = reversed * 10 + pop;
-  //   x = Math.floor(x / 10);
-  // }
-
-  // return reversed === original;
+  return reversed === original;
 }
