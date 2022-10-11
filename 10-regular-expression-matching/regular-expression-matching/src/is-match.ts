6f4a8b99be84090526e47c27562ec912c50699a6
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-10-11 18:41:26 -0500

    TS - Regex Matching - Solve the regular expression problem again

---
+++ 10-regular-expression-matching/regular-expression-matching/src/is-match.ts
@@ -0,0 +1,45 @@
+let _result: Result[][];
+
+export function isMatch(s: string, p: string): boolean {
+    _result = instantiateResult(s.length, p.length);
+    return isMatchExpression(s, p, 0, 0);
+}
+
+function isMatchExpression(s: string, p: string, i: number, j: number) {
+    if (_result[i][j] !== Result.Undefined) {
+        return this._result[i][j] === Result.True;
+    }
+
+    if (j >= p.length) {
+        return i >= s.length;
+    }
+
+    let result: boolean;
+    const firstMatch = i < s.length && (s.charAt(i) === p.charAt(j) || p.charAt(j) === '.');
+
+    if (j + 1 < p.length && p[j + 1] === '*') {
+        result = (isMatchExpression(s, p, i, j + 2) || (firstMatch && isMatchExpression(s, p, i + 1, j)));
+    } else {
+        result = firstMatch && isMatchExpression(s, p, i + 1, j + 1);
+    }
+
+    _result[i][j] = result ? Result.True : Result.False;
+    return result;
+}
+
+function instantiateResult(sLength: number, pLength: number): Result[][] {
+    const result = new Array(sLength + 1)
+    for (let i = 0; i < result.length; i++) {
+        result[i] = new Array(pLength + 1);
+        for (let j = 0; j < result[i].length; j++) {
+            result[i][j] = Result.Undefined;
+        }
+    }
+    return result;
+}
+
+enum Result {
+    Undefined,
+    True,
+    False,
+}
--- 10-regular-expression-matching/regular-expression-matching/tests/is-match.test.ts
+++ 10-regular-expression-matching/regular-expression-matching/tests/is-match.test.ts
@@ -1,27 +1,23 @@
-import {ExpressionMatching} from '../src/is-match'
+import {isMatch} from '../src/is-match'
 
 describe('isMatch', function() {
-  let expressionMatching: ExpressionMatching;
-
-  beforeEach(() => expressionMatching = new ExpressionMatching)
-
   it('is passed "aa" and "a" and returns false', function() {
-    const result = expressionMatching.isMatch('aa', 'a')
+    const result = isMatch('aa', 'a')
     expect(result).toBe(false)
   })
 
   it('is passed "aa" and "a*" and returns true', function() {
-    const result = expressionMatching.isMatch('aa', 'a*')
+    const result = isMatch('aa', 'a*')
     expect(result).toBe(true)
   })
 
   it('is passed "ab" and ".*" and returns true', function() {
-    const result = expressionMatching.isMatch("ab", ".*")
+    const result = isMatch("ab", ".*")
     expect(result).toBe(true)
   })
 
   it('is passed "ab" and ".*c" and returns false', function() {
-    const result = expressionMatching.isMatch("ab", ".*c")
+    const result = isMatch("ab", ".*c")
     expect(result).toBe(false)
   })
 })
