c90bc9422ea7ebba184854b7135893a945d4e1e9
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-10-12 18:07:25 -0500

    dotnet - Palindrome Number - Solve again

--- 9-palindrome-number/PalindromeNumber/PalindromeNumber/PalindromeLibrary.cs
+++ 9-palindrome-number/PalindromeNumber/PalindromeNumber/PalindromeLibrary.cs
@@ -1,23 +1,19 @@
-namespace PalindromeNumber;
-
 public class PalindromeLibrary
 {
     public bool IsPalindrome(int x)
     {
         if (x < 0) return false;
 
-        if (x == 0) return true;
-
         var original = x;
         var reversed = 0;
 
-        while (x != 0)
+        while (x > 0)
         {
             var pop = x % 10;
             reversed = reversed * 10 + pop;
             x /= 10;
         }
 
-        return reversed == original;
-    } 
+        return original == reversed;
+    }
 }
