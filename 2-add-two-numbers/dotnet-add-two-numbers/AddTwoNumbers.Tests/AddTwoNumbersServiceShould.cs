740231a8658e61c442d869ed2f77303cb8a3f3d3
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-11-06 08:44:56 -0500

    dotnet - Add Two Numbers - Move Namespace to file namespace

--- 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers.Tests/AddTwoNumbersServiceShould.cs
+++ 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers.Tests/AddTwoNumbersServiceShould.cs
@@ -1,83 +1,82 @@
 using NUnit.Framework;
 
-namespace AddTwoNumbers.Tests
+namespace AddTwoNumbers.Tests;
+
+public class AddTwoNumbersServiceShould
 {
-    public class AddTwoNumbersServiceShould
+    private AddTwoNumbersService _addTwoNumbersService;
+
+    [SetUp]
+    public void SetUp()
     {
-        private AddTwoNumbersService _addTwoNumbersService;
+        _addTwoNumbersService = new AddTwoNumbersService();
+    }
 
-        [SetUp]
-        public void SetUp()
+    [Test]
+    public void AddTwoNumbers_InputTwoArraysOfDiffDigits_ReturnCorrectSumInReverse()
+    {
+        var listOneArr = new[] {2, 4, 3};
+        var listTwoArr = new[] {5, 6, 4};
+
+        var listOne = ArrayToLinkedList(listOneArr);
+        var listTwo = ArrayToLinkedList(listTwoArr);
+
+        var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
+
+        var expectedAnswerArr = new[] {7, 0, 8};
+        var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
+
+        Assert.True(result.Equals(expectedAnswer));
+    }
+
+    [Test]
+    public void AddTwoNumbers_InputTwoArraysOfZero_ReturnZero()
+    {
+        var listOneArr = new[] {0};
+        var listTwoArr = new[] {0};
+
+        var listOne = ArrayToLinkedList(listOneArr);
+        var listTwo = ArrayToLinkedList(listTwoArr);
+
+        var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
+
+        var expectedAnswerArr = new[] {0};
+        var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
+
+        Assert.True(result.Equals(expectedAnswer));
+    }
+
+    [Test]
+    public void AddNumbers_InputTwoArraysWithCarryOver_ReturnCorrectList()
+    {
+        var listOneArr = new[] {9, 9, 9, 9, 9, 9, 9};
+        var listTwoArr = new[] {9, 9, 9, 9};
+
+        var listOne = ArrayToLinkedList(listOneArr);
+        var listTwo = ArrayToLinkedList(listTwoArr);
+
+        var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
+
+        var expectedAnswerArr = new[] {8, 9, 9, 9, 0, 0, 0, 1};
+        var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
+
+        Assert.True(result.Equals(expectedAnswer));
+    }
+
+    private ListNode ArrayToLinkedList(int[] listOneArr)
+    {
+        var firstNode = new ListNode(listOneArr[0]);
+        var currentNode = firstNode;
+
+        for (var index = 1; index < listOneArr.Length; index++)
         {
-            _addTwoNumbersService = new AddTwoNumbersService();
+            var item = listOneArr[index];
+            var node = new ListNode(item);
+
+            currentNode.next = node;
+            currentNode = node;
         }
 
-        [Test]
-        public void AddTwoNumbers_InputTwoArraysOfDiffDigits_ReturnCorrectSumInReverse()
-        {
-            var listOneArr = new[] {2, 4, 3};
-            var listTwoArr = new[] {5, 6, 4};
-
-            var listOne = ArrayToLinkedList(listOneArr);
-            var listTwo = ArrayToLinkedList(listTwoArr);
-
-            var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
-
-            var expectedAnswerArr = new[] {7, 0, 8};
-            var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
-
-            Assert.True(result.Equals(expectedAnswer));
-        }
-
-        [Test]
-        public void AddTwoNumbers_InputTwoArraysOfZero_ReturnZero()
-        {
-            var listOneArr = new[] {0};
-            var listTwoArr = new[] {0};
-
-            var listOne = ArrayToLinkedList(listOneArr);
-            var listTwo = ArrayToLinkedList(listTwoArr);
-
-            var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
-
-            var expectedAnswerArr = new[] {0};
-            var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
-
-            Assert.True(result.Equals(expectedAnswer));
-        }
-
-        [Test]
-        public void AddNumbers_InputTwoArraysWithCarryOver_ReturnCorrectList()
-        {
-            var listOneArr = new[] {9, 9, 9, 9, 9, 9, 9};
-            var listTwoArr = new[] {9, 9, 9, 9};
-
-            var listOne = ArrayToLinkedList(listOneArr);
-            var listTwo = ArrayToLinkedList(listTwoArr);
-
-            var result = _addTwoNumbersService.AddTwoNumbers(listOne, listTwo);
-
-            var expectedAnswerArr = new[] {8, 9, 9, 9, 0, 0, 0, 1};
-            var expectedAnswer = ArrayToLinkedList(expectedAnswerArr);
-
-            Assert.True(result.Equals(expectedAnswer));
-        }
-
-        private ListNode ArrayToLinkedList(int[] listOneArr)
-        {
-            var firstNode = new ListNode(listOneArr[0]);
-            var currentNode = firstNode;
-
-            for (var index = 1; index < listOneArr.Length; index++)
-            {
-                var item = listOneArr[index];
-                var node = new ListNode(item);
-
-                currentNode.next = node;
-                currentNode = node;
-            }
-
-            return firstNode;
-        }
+        return firstNode;
     }
 }
