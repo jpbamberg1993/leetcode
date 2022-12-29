5966ae9881d9b7da15827fa82b49df5c1c026a00
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-12-29 11:09:30 -0500

    dotnet - Add Two Linked Lists - Solve again

--- 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers/AddTwoNumbersService.cs
+++ 2-add-two-numbers/dotnet-add-two-numbers/AddTwoNumbers/AddTwoNumbersService.cs
@@ -2,21 +2,23 @@ namespace AddTwoNumbers;
 
 public class AddTwoNumbersService
 {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+  public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+  {
+    var currentHead = new ListNode(0);
+    var dummyHead = currentHead;
+    var carry = 0;
+    while (l1 != null || l2 != null || carry != 0)
     {
-        var dummyHead = new ListNode(0);
-        var currentHead = dummyHead;
-        var carry = 0;
-        while (l1 != null || l2 != null || carry != 0)
-        {
-            var value = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
-            carry = value >= 10 ? 1 : 0;
-            currentHead.next = new ListNode(value % 10);
-            currentHead = currentHead.next;
-            l1 = l1?.next;
-            l2 = l2?.next;
-        }
-
-        return dummyHead.next;
+      var val1 = l1?.val ?? 0;
+      var val2 = l2?.val ?? 0;
+      var val = val1 + val2 + carry;
+      currentHead.next = new ListNode(val % 10);
+      currentHead = currentHead.next;
+      carry = val > 9 ? 1 : 0;
+      l1 = l1?.next;
+      l2 = l2?.next;
     }
+
+    return dummyHead.next;
+  }
 }
