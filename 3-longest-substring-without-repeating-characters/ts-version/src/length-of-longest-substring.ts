a794e25837a55f0a4de1c131e15de590645ab45c
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-01-19 07:44:09 -0500

    TS - Longest substring wo repeating chars - Solve again

--- 3-longest-substring-without-repeating-characters/ts-version/src/length-of-longest-substring.ts
+++ 3-longest-substring-without-repeating-characters/ts-version/src/length-of-longest-substring.ts
@@ -1,26 +1,22 @@
 export function lengthOfLongestSubstring(s: string): number {
-    if (s.length < 1) return 0;
+  const letterCountDict = new Map<string, number>();
+  let result = 0;
+  let start = 0;
+  let end = 0;
 
-    const chars = new Map<string, number>();
+  while (end < s.length) {
+    let r: string = s.charAt(end)
+    letterCountDict.set(r, (letterCountDict.get(r) ?? 0) + 1)
 
-    let result = 0;
-    let start = 0;
-    let end = 0;
-
-    while (end < s.length) {
-        const r = s.charAt(end);
-
-        chars.set(r, chars.get(r) + 1 || 1);
-
-        while (chars.get(r) > 1) {
-            const l: string = s[start];
-            chars.set(l, chars.get(l) - 1);
-            start++;
-        }
-
-        result = Math.max(result, end - start + 1);
-        end++;
+    while (letterCountDict.get(r)! > 1) {
+      const l = s.charAt(start);
+      letterCountDict.set(l, letterCountDict.get(l)! - 1)
+      start++;
     }
 
-    return result;
+    result = Math.max(end - start + 1, result)
+    end++
+  }
+
+  return result;
 }
