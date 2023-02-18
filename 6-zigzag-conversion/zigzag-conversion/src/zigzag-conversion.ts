6e6271b42e84f62850444a119028eb1755fa86ed
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-02-18 13:46:32 -0500

    TS - Zigzag Conversion - Solve again

--- 6-zigzag-conversion/zigzag-conversion/src/zigzag-conversion.ts
+++ 6-zigzag-conversion/zigzag-conversion/src/zigzag-conversion.ts
@@ -1,22 +1,21 @@
 export function zigzagConversion(s: string, numRows: number): string {
-    if (numRows <= 1) return s;
+  if (numRows <= 1) {
+    return s
+  }
 
-    const zigzag = Array<string>(numRows);
-    for (let i = 0; i < zigzag.length; i++) {
-        zigzag[i] = '';
+  const response: string[] = new Array(numRows).fill('')
+  let goingDown: boolean = false
+  let currentRow: number = 0
+
+  for (let i = 0; i < s.length; i++) {
+    response[currentRow] += s[i]
+
+    if (currentRow === 0 || currentRow === numRows - 1) {
+      goingDown = !goingDown
     }
 
-    let goingDown = false;
-    let currentRow = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (currentRow === 0 || currentRow === numRows - 1) {
-            goingDown = !goingDown;
-        }
+    currentRow += goingDown ? 1 : -1
+  }
 
-        zigzag[currentRow] = zigzag[currentRow] + s.charAt(i);
-
-        currentRow += goingDown ? 1 : -1;
-    }
-
-    return zigzag.join('');
+  return response.join('')
 }
