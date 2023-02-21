fe3d06fb45a29c71f6e9fe01be4c670107870640
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-02-21 09:28:49 -0500

    TS - Rever Integer - Solve again

--- 7-reverse-integer/reverse-integer/src/reverse-integer.ts
+++ 7-reverse-integer/reverse-integer/src/reverse-integer.ts
@@ -1,18 +1,14 @@
-const INT_MAX = Math.pow(2, 31) - 1;
-const INT_MIN = Math.pow(-2, 31) - 1;
-
 export function reverseInteger(x: number): number {
-    let reversed = 0;
+    let result: number = 0;
     while (x !== 0) {
-        const pop = x % 10;
-        x = Math.trunc(x / 10);
-        if (reversed > Math.floor(INT_MAX / 10) || (reversed === Math.floor(INT_MAX / 10) && pop > INT_MAX % 10)) {
-            return 0;
-        }
-        if (reversed < Math.ceil(INT_MIN / 10) || (reversed === Math.ceil(INT_MIN / 10) && pop < INT_MIN % 10)) {
-            return 0;
-        }
-        reversed = reversed * 10 + pop;
+        const num = x % 10;
+        if (result * 10 > INT_MAX || (result === INT_MAX / 10 && num > INT_MAX % 10)) return 0
+        if (result * 10 < INT_MIN || (result === INT_MIN / 10 && num < INT_MIN % 10)) return 0
+        result = result * 10 + num
+        x = Math.trunc(x / 10)
     }
-    return reversed;
+    return result
 }
+
+const INT_MAX = Math.pow(2, 31) - 1
+const INT_MIN = Math.pow(-2, 31)
