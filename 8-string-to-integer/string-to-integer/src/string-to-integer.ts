cedabcf585eed49fcb1d2b2e8fd32f0c6f1b0184
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-03-14 20:21:44 -0400

    Solve string to integer using a state machine

--- 8-string-to-integer/string-to-integer/src/string-to-integer.ts
+++ 8-string-to-integer/string-to-integer/src/string-to-integer.ts
@@ -1,101 +1,94 @@
 export function myAtoi(s: string): number {
-    const stateMachine = new StateMachine();
+    const stateMachine = stateMachineFactory()
     for (let i = 0; i < s.length && stateMachine.getState() !== State.Qd; i++) {
-        stateMachine.transition(s.charAt(i));
+        stateMachine.transition(s[i])
     }
-    return stateMachine.getValue();
+    return stateMachine.getResult()
 }
 
-const INT_MAX = Math.pow(2, 31) - 1;
-const INT_MIN = Math.pow(-2, 31);
+function stateMachineFactory(): StateMachine {
+    let state: State = State.Q0
+    let result: number = 0
+    let carry: number = 1
 
-class StateMachine {
-    private state: State;
-    private value: number;
-    private symbol: number;
-
-    constructor() {
-        this.state = State.Q0;
-        this.value = 0;
-        this.symbol = 1;
+    return {
+        getState,
+        getResult,
+        transition,
     }
 
-    public getState(): State {
-        return this.state;
+    function getState(): State {
+        return state
     }
 
-    public getValue(): number {
-        return this.value * this.symbol;
+    function getResult(): number {
+        return carry * result
     }
 
-    public transition(ch: string): void {
-        if (this.state === State.Q0) {
-            if (ch === ' ') {
-                return;
-            } else if (ch === '+' || ch === '-') {
-                this.toStateQ1(ch);
-            } else if (this.isNumeric(ch)) {
-                this.toStateQ2(ch);
+    function transition(c: string): void {
+        if (state === State.Q0) {
+            if (c === ' ') {
+                return
+            } else if (charIsDigit(c)) {
+                toStateQ2(c)
+            } else if (c === '-' || c === '+') {
+                toStateQ1(c)
             } else {
-                this.toStateQd();
+                toStateQd()
             }
         } else {
-            if (this.isNumeric(ch)) {
-                this.toStateQ2(ch);
+            if (charIsDigit(c)) {
+                toStateQ2(c)
             } else {
-                this.toStateQd();
+                toStateQd()
             }
         }
     }
 
-    private toStateQ1(ch: string): void {
-        if (ch === '-') {
-            this.symbol = -1;
+    function toStateQ1(c: string): void {
+        if (c === '-') {
+            carry = -1
         }
-        this.state = State.Q1;
+        state = State.Q1
     }
 
-    private toStateQ2(ch: string): void {
-        const digit = Number(ch);
-        this.state = State.Q2;
-        this.appendDigit(digit);
+    function toStateQ2(c: string): void {
+        state = State.Q2
+        const value = parseInt(c, 10)
+        appendDigit(value)
     }
 
-    private toStateQd(): void {
-        this.state = State.Qd;
+    function toStateQd() {
+        state = State.Qd
     }
 
-    private appendDigit(digit: number): void {
-        if (this.value > Math.floor(INT_MAX / 10) ||
-            (this.value === Math.floor(INT_MAX / 10) && digit > INT_MAX % 10)) {
-            if (this.symbol === -1) {
-                this.value = INT_MIN;
-                this.symbol = 1;
-            } else {
-                this.value = INT_MAX;
-            }
-            this.toStateQd();
-        } else {
-            this.value = this.value * 10 + digit;
+    function appendDigit(d: number): void {
+        if ((result > INT_MAX / 10 || (result === Math.trunc(INT_MAX / 10) && d > INT_MAX % 10))) {
+            result = carry < 0 ? INT_MIN : INT_MAX
+            carry = 1
+            toStateQd()
+            return
         }
-    }
-
-    private isNumeric(ch: string): boolean {
-        if (typeof ch !== 'string') {
-            return false;
-        }
-
-        if (ch.trim() === '') {
-            return false;
-        }
-
-        return !isNaN(+ch);
+        result = result * 10 + d
     }
 }
+
+function charIsDigit(c: string): boolean {
+    return c >= '0' && c <= '9'
+}
+
+const INT_MAX = Math.pow(2, 31) - 1
+const INT_MIN = Math.pow(-2, 31)
 
 enum State {
     Q0,
     Q1,
     Q2,
     Qd,
+}
+
+type StateMachine = {
+    getState: () => State
+    getResult: () => number
+    transition: (c: string) => void
 }
