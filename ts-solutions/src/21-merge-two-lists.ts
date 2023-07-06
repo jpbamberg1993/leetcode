f3005a27218a73e5cc5bebe87762ad6173924220
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-07-06 10:34:43 -0500

    21. Merge Two Lists - TS - Solve again using recursion

--- ts-solutions/src/21-merge-two-lists.ts
+++ ts-solutions/src/21-merge-two-lists.ts
@@ -4,32 +4,21 @@ export function mergeTwoLists(
 	list1: ListNode | null,
 	list2: ListNode | null
 ): ListNode | null {
-	const dummyHead = new ListNode()
-	let currentHead: ListNode | null = dummyHead
-	while (list1 || list2) {
-		if (list1 === null) {
-			currentHead.next = list2
-			currentHead = currentHead.next
-			break
-		}
-		if (list2 === null) {
-			currentHead.next = list1
-			currentHead = currentHead.next
-			break
-		}
-		if (list1 != null && list1.val <= list2.val) {
-			currentHead.next = new ListNode(list1.val)
-			list1 = list1.next
-			currentHead = currentHead.next
-			continue
-		}
-		if (list2 != null && list2.val <= list1.val) {
-			currentHead.next = new ListNode(list2.val)
-			list2 = list2.next
-			currentHead = currentHead.next
-			continue
-		}
+	if (list1 == null || list2 == null) {
+		return list1 ?? list2
 	}
 
-	return dummyHead.next
+	let head: ListNode
+
+	if (list1.val <= list2.val) {
+		head = list1
+		list1 = list1?.next
+	} else {
+		head = list2
+		list2 = list2?.next
+	}
+
+	head.next = mergeTwoLists(list1, list2)
+
+	return head
 }
