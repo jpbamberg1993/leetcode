1da1b726db53d257242f5f3035b2a370df977802
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-07-06 15:27:17 -0500

    21. Merge Two Sorted Lists - TS - Solve using pointers instead of recursion

--- ts-solutions/src/21-merge-two-lists.ts
+++ ts-solutions/src/21-merge-two-lists.ts
@@ -4,21 +4,24 @@ export function mergeTwoLists(
 	list1: ListNode | null,
 	list2: ListNode | null
 ): ListNode | null {
-	if (list1 == null || list2 == null) {
-		return list1 ?? list2
+	const head = new ListNode()
+	let tail = head
+	while (list1 || list2) {
+		if (list1 == null || list2 == null) {
+			tail.next = list1 ?? list2
+			break
+		}
+
+		if (list1.val <= list2.val) {
+			tail.next = list1
+			list1 = list1.next
+		} else {
+			tail.next = list2
+			list2 = list2.next
+		}
+
+		tail = tail.next
 	}
 
-	let head: ListNode
-
-	if (list1.val <= list2.val) {
-		head = list1
-		list1 = list1?.next
-	} else {
-		head = list2
-		list2 = list2?.next
-	}
-
-	head.next = mergeTwoLists(list1, list2)
-
-	return head
+	return head.next
 }
