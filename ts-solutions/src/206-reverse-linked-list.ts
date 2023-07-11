4473406dcf4f889e4a1bf0447bb5300c03a22acd
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-07-11 08:03:12 -0400

    206. Reverse Linked List - Write cleaner solution

--- ts-solutions/src/206-reverse-linked-list.ts
+++ ts-solutions/src/206-reverse-linked-list.ts
@@ -2,12 +2,13 @@ import { ListNode } from '../utils/list-node'
 
 export function reverseList(head: ListNode | null): ListNode | null {
 	if (!head) return null
-	let currentNode: ListNode | null = new ListNode(head?.val)
-	head = head?.next ?? null
-	while (head) {
-		const node = new ListNode(head.val, currentNode)
-		currentNode = node
-		head = head.next
+	let previousNode: ListNode | null = null
+	let currentNode: ListNode | null = head
+	while (currentNode) {
+		const currentNodeNext = currentNode?.next
+		currentNode.next = previousNode
+		previousNode = currentNode
+		currentNode = currentNodeNext
 	}
-	return currentNode
+	return previousNode
 }
