f9adb23bdb51b0cbc4316e69ec6319e070cf0806
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-07-27 07:37:33 -0400

    205 Isomorphic Strings - TS - Code cleanup

--- ts-solutions/src/205-isomorphic-strings.ts
+++ ts-solutions/src/205-isomorphic-strings.ts
@@ -3,22 +3,14 @@ export function isIsomorphic(s: string, t: string): boolean {
 	const tToSMap = new Map<string, string>()
 
 	for (let i = 0; i < s.length; i++) {
-		if (sToTMap.has(s[i]) || tToSMap.has(t[i])) {
-			const sReplacement = sToTMap.get(s[i]) ?? ``
-			const tReplacement = tToSMap.get(sReplacement)
-
-			if (tReplacement !== s[i]) {
-				return false
-			}
-
-			if (sReplacement === t[i] && tReplacement === s[i]) {
-				continue
-			} else {
-				return false
-			}
-		} else {
+		if (!sToTMap.has(s[i]) && !tToSMap.has(t[i])) {
 			sToTMap.set(s[i], t[i])
 			tToSMap.set(t[i], s[i])
+			continue
+		}
+
+		if (sToTMap.get(s[i]) !== t[i] || tToSMap.get(t[i]) !== s[i]) {
+			return false
 		}
 	}
 
