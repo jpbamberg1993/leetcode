8c45c1d0e6469a3440b0bdd65ce77eef34b3754e
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-07-29 08:48:31 -0400

    20 Merge Two Lists - Solve using iteration

--- ts-solutions/src/21-merge-two-lists.ts
+++ ts-solutions/src/21-merge-two-lists.ts
@@ -4,23 +4,21 @@ export function mergeTwoLists(
 	list1: ListNode | null,
 	list2: ListNode | null
 ): ListNode | null {
-	const head = new ListNode()
-	let tail = head
-	while (list1 != null && list2 != null) {
+	const dummyHead = new ListNode()
+	let resultNode = dummyHead
+	while (list1 && list2) {
 		if (list1.val <= list2.val) {
-			tail.next = list1
+			resultNode.next = list1
 			list1 = list1.next
 		} else {
-			tail.next = list2
+			resultNode.next = list2
 			list2 = list2.next
 		}
-
-		tail = tail.next
+		resultNode = resultNode.next
+	}
+	if (list1 || list2) {
+		resultNode.next = list1 ?? list2
 	}
 
-	if (list1 == null || list2 == null) {
-		tail.next = list1 ?? list2
-	}
-
-	return head.next
+	return dummyHead.next
 }
