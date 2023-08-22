f473c92c588af1d2be4295c9831c7aa1c80d1d6d
Paul Bamberg <jpbamberg1993@gmail.com>
2023-08-22 05:33:01 -0500

    206 - reverse linked list - solve by changing pointers

--- ts-solutions/src/206-reverse-linked-list.ts
+++ ts-solutions/src/206-reverse-linked-list.ts
@@ -1,14 +1,13 @@
 import { ListNode } from '../utils/list-node'
 
 export function reverseList(head: ListNode | null): ListNode | null {
-	if (!head) return null
 	let previousNode: ListNode | null = null
 	let currentNode: ListNode | null = head
 	while (currentNode) {
-		const currentNodeNext = currentNode?.next
+		const next = currentNode.next
 		currentNode.next = previousNode
 		previousNode = currentNode
-		currentNode = currentNodeNext
+		currentNode = next
 	}
 	return previousNode
 }
