bfc63cc3fac2141889ac8a409003538906b9065c
Paul Bamberg <jpbamberg1993@gmail.com>
2023-09-17 13:04:55 -0500

    Improve code readability

--- go-solutions/gcd_of_strings.go
+++ go-solutions/gcd_of_strings.go
@@ -9,16 +9,18 @@ func GcdOfString(str1 string, str2 string) string {
 	len1 := len(str1)
 	len2 := len(str2)
 
-	validString := func(substring string, k int) bool {
+	valid := func(substring string, k int) bool {
 		if len1%k != 0 || len2%k != 0 {
 			return false
 		}
-		return repeatString(substring, len1/k) == str1 && repeatString(substring, len2/k) == str2
+		n1 := len1 / k
+		n2 := len2 / k
+		return repeatString(substring, n1) == str1 && repeatString(substring, n2) == str2
 	}
 
 	for i := int(math.Min(float64(len1), float64(len2))); i > 0; i-- {
 		substring := str1[0:i]
-		if validString(substring, i) {
+		if valid(substring, i) {
 			return substring
 		}
 	}
