db59e3319f3a8d1e3dbd834eae193a52e6fb77f3
Paul Bamberg <jpbamberg1993@gmail.com>
2023-10-23 18:34:11 -0500

    605. Can Place Flowers - Solve again but reduce memory consumption by not updating values in the array

--- go-solutions/can_place_flowers.go
+++ go-solutions/can_place_flowers.go
@@ -3,18 +3,23 @@ package leetcode
 func CanPlaceFlowers(flowerbed []int, n int) bool {
 	flowerbedLen := len(flowerbed)
 	count := 0
-	for i := 0; i < flowerbedLen; i++ {
-		if flowerbed[i] == 1 {
+	currentIndex := 0
+	for currentIndex < flowerbedLen {
+		if flowerbed[currentIndex] == 1 {
+			currentIndex++
 			continue
 		}
-		emptyLeftPlot := (i == 0) || (flowerbed[i-1] == 0)
-		emptyRightPlot := (i == flowerbedLen-1) || (flowerbed[i+1] == 0)
+		emptyLeftPlot := (currentIndex == 0) || (flowerbed[currentIndex-1] == 0)
+		emptyRightPlot := (currentIndex == flowerbedLen-1) || (flowerbed[currentIndex+1] == 0)
 		if emptyLeftPlot && emptyRightPlot {
-			flowerbed[i] = 1
+			flowerbed[currentIndex] = 1
 			count++
 			if count >= n {
 				return true
 			}
+			currentIndex += 2
+		} else {
+			currentIndex += 1
 		}
 	}
 	return count >= n
