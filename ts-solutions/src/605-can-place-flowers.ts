bfcb31bbf32d7337f48c129ac03d3611ac48e017
Paul Bamberg <jpbamberg1993@gmail.com>
2023-10-23 18:48:26 -0500

    605. Can Place Flowers - Initial typescript solution

--- ts-solutions/src/605-can-place-flowers.ts
+++ ts-solutions/src/605-can-place-flowers.ts
@@ -1,7 +1,16 @@
 export function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+	let count = 0
 	for (let i = 0; i < flowerbed.length; i++) {
 		if (flowerbed[i] === 1) {
 			continue
 		}
+		const leftSideEmpty = i === 0 || flowerbed[i - 1] === 0
+		const rightSideEmpty = i === flowerbed.length - 1 || flowerbed[i + 1] === 0
+		if (leftSideEmpty && rightSideEmpty) {
+			flowerbed[i] = 1
+			count++
+			if (count >= n) return true
+		}
 	}
+	return count >= n
 }
