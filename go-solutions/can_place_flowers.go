6f843321400b91f6b82dfeecce8a1a436d26bfbd
Paul Bamberg <jpbamberg1993@gmail.com>
2023-10-23 18:07:39 -0500

    605. Can Place Flowers - Refactor to be more readible

--- go-solutions/can_place_flowers.go
+++ go-solutions/can_place_flowers.go
@@ -1,31 +1,21 @@
 package leetcode
 
-import "log"
-
 func CanPlaceFlowers(flowerbed []int, n int) bool {
-	if n == 0 {
-		return true
-	}
-
-	currentCount := 0
-	currentIndex := 0
-	for currentIndex < len(flowerbed) {
-		if flowerbed[currentIndex] == 1 {
-			currentIndex++
+	flowerbedLen := len(flowerbed)
+	count := 0
+	for i := 0; i < flowerbedLen; i++ {
+		if flowerbed[i] == 1 {
 			continue
 		}
-		if (currentIndex == 0 || flowerbed[currentIndex-1] == 0) &&
-			(currentIndex == len(flowerbed)-1 || (currentIndex+1 < len(flowerbed) && flowerbed[currentIndex+1] == 0)) {
-			log.Printf("--> Current index set to true: %v\n", currentIndex)
-			currentCount++
-			currentIndex += 2
-		} else {
-			currentIndex++
-		}
-
-		if currentCount == n {
-			return true
+		emptyLeftPlot := (i == 0) || (flowerbed[i-1] == 0)
+		emptyRightPlot := (i == flowerbedLen-1) || (flowerbed[i+1] == 0)
+		if emptyLeftPlot && emptyRightPlot {
+			flowerbed[i] = 1
+			count++
+			if count >= n {
+				return true
+			}
 		}
 	}
-	return currentCount == n
+	return count >= n
 }
