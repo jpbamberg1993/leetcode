3d71483bf63cf8949d27265206bc969989f406b3
Paul Bamberg <jpbamberg1993@gmail.com>
2024-01-20 11:28:58 -0500

    1768 Merge Strings Alternatly - TS - Initial solution using fori

--- ts-solutions/src/1768-merge-strings-alternatly.ts
+++ ts-solutions/src/1768-merge-strings-alternatly.ts
@@ -1,19 +1,12 @@
 export function mergeAlternately(word1: string, word2: string): string {
-	const m = word1.length
-	const n = word2.length
-	const response = new Array(m + n)
-	let responseIndex = 0
-
-	for (let i = 0; i < Math.max(m, n); i++) {
-		if (i < m) {
-			response[responseIndex] = word1[i]
-			responseIndex++
+	let result = ``
+	for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+		if (i < word1.length) {
+			result += word1[i]
 		}
-		if (i < n) {
-			response[responseIndex] = word2[i]
-			responseIndex++
+		if (i < word2.length) {
+			result += word2[i]
 		}
 	}
-
-	return response.join('')
+	return result
 }
--- ts-solutions/tests/1768-merge-strings-alternatly.test.ts
+++ ts-solutions/tests/1768-merge-strings-alternatly.test.ts
@@ -2,17 +2,17 @@ import { mergeAlternately } from '../src/1768-merge-strings-alternatly'
 
 describe(`mergeAlternately`, () => {
 	it(`is passed 'abc', 'pqr' => 'apbqcr'`, () => {
-		const result = mergeAlternately('abc', 'pqr')
-		expect(result).toBe('apbqcr')
+		const result = mergeAlternately(`abc`, `pqr`)
+		expect(result).toBe(`apbqcr`)
 	})
 
 	it(`is passed 'ab', 'pqrs' => 'apbqrs'`, () => {
-		const result = mergeAlternately('ab', 'pqrs')
-		expect(result).toBe('apbqrs')
+		const result = mergeAlternately(`ab`, `pqrs`)
+		expect(result).toBe(`apbqrs`)
 	})
 
 	it(`is passed 'abcd', 'pq' => 'apbqcd'`, () => {
-		const result = mergeAlternately('abcd', 'pq')
-		expect(result).toBe('apbqcd')
+		const result = mergeAlternately(`abcd`, `pq`)
+		expect(result).toBe(`apbqcd`)
 	})
 })
