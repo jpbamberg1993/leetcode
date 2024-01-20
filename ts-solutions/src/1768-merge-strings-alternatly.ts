04f4d3430fd8800830d1802cd0b3faf8b9a241e5
Paul Bamberg <jpbamberg1993@gmail.com>
2024-01-20 11:52:47 -0500

    1768. Merge Strings Alternatly - Improve efficiency by not looping threw longest string

--- ts-solutions/src/1768-merge-strings-alternatly.ts
+++ ts-solutions/src/1768-merge-strings-alternatly.ts
@@ -1,12 +1,18 @@
 export function mergeAlternately(word1: string, word2: string): string {
 	let result = ``
-	for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
-		if (i < word1.length) {
-			result += word1[i]
-		}
-		if (i < word2.length) {
-			result += word2[i]
-		}
+	let currentIndex = 0
+	const word1Len = word1.length
+	const word2Len = word2.length
+	while (currentIndex < word1Len && currentIndex < word2Len) {
+		result += word1[currentIndex]
+		result += word2[currentIndex]
+		currentIndex++
+	}
+	if (currentIndex < word1Len) {
+		result += word1.substring(currentIndex)
+	}
+	if (currentIndex < word2Len) {
+		result += word2.substring(currentIndex)
 	}
 	return result
 }
