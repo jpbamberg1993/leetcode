06f1c9466ff2ed46619fe90c359103d5d80a5682
Paul Bamberg <jpbamberg1993@gmail.com>
2024-01-23 18:02:40 -0500

    1071. Greatest Common Divisor - Solve again by finding the greatest common divisor

--- ts-solutions/src/1071-greatest-common-divisor-of-strings.ts
+++ ts-solutions/src/1071-greatest-common-divisor-of-strings.ts
@@ -1,16 +1,16 @@
 export function gcdOfStrings(str1: string, str2: string): string {
 	if (str1 + str2 !== str2 + str1) {
 		return ``
+	} else {
+		const separator = gcd(str1.length, str2.length)
+		return str1.substring(0, separator)
 	}
-
-	const gcdLength = gcd(str1.length, str2.length)
-	return str1.substring(0, gcdLength)
 }
 
-function gcd(x: number, y: number) {
-	if (y === 0) {
-		return x
+function gcd(len1: number, len2: number): number {
+	if (len2 === 0) {
+		return len1
 	} else {
-		return gcd(y, x % y)
+		return gcd(len2, len1 % len2)
 	}
 }
--- ts-solutions/tests/1071-greatest-common-divisor-of-strings.test.ts
+++ ts-solutions/tests/1071-greatest-common-divisor-of-strings.test.ts
@@ -1,19 +1,19 @@
 import { gcdOfStrings } from '../src/1071-greatest-common-divisor-of-strings'
 
-describe('gcdOfStrings', () => {
-	it('is passed str1 = "ABCABC", str2 = "ABC" => "ABC"', () => {
-		expect(gcdOfStrings('ABCABC', 'ABC')).toBe('ABC')
+describe(`gcdOfStrings`, () => {
+	it(`is passed str1 = "ABCABC", str2 = "ABC" => "ABC"`, () => {
+		expect(gcdOfStrings(`ABCABC`, `ABC`)).toBe(`ABC`)
 	})
 
-	it('is passed str1 = "ABABAB", str2 = "ABAB" => "AB"', () => {
-		expect(gcdOfStrings('ABABAB', 'ABAB')).toBe('AB')
+	it(`is passed str1 = "ABABAB", str2 = "ABAB" => "AB"`, () => {
+		expect(gcdOfStrings(`ABABAB`, `ABAB`)).toBe(`AB`)
 	})
 
-	it('str1 = "LEET", str2 = "CODE" => ""', () => {
-		expect(gcdOfStrings('LEET', 'CODE')).toBe('')
+	it(`str1 = "LEET", str2 = "CODE" => ""`, () => {
+		expect(gcdOfStrings(`LEET`, `CODE`)).toBe(``)
 	})
 
-	it('is passed str1 = "AA", str2 = "A" => "A"', () => {
-		expect(gcdOfStrings('AA', 'A')).toBe('A')
+	it(`is passed str1 = "AA", str2 = "A" => "A"`, () => {
+		expect(gcdOfStrings(`AA`, `A`)).toBe(`A`)
 	})
 })
