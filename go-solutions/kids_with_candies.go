28208f79ead8129cfbe089b72f7c235c540bb2f1
Paul Bamberg <jpbamberg1993@gmail.com>
2024-01-23 19:00:45 -0500

    1431. Kids With the Greatest Number of Candies - Solve again using two loops

--- go-solutions/kids_with_candies.go
+++ go-solutions/kids_with_candies.go
@@ -7,8 +7,7 @@ func KidsWithCandies(candies []int, extraCandies int) []bool {
 			maxCandies = candy
 		}
 	}
-
-	var result = make([]bool, len(candies))
+	result := make([]bool, len(candies))
 	for i, candy := range candies {
 		result[i] = candy+extraCandies >= maxCandies
 	}
