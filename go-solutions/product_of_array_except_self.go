2873198d9857f036719a2364171bb4e7aeac5331
Paul Bamberg <jpbamberg1993@gmail.com>
2024-03-30 22:11:04 -0400

    238. Product of Array Except Self - Go - Solve using one array

--- go-solutions/product_of_array_except_self.go
+++ go-solutions/product_of_array_except_self.go
@@ -2,19 +2,15 @@ package leetcode
 
 func ProductExceptSelf(nums []int) []int {
 	numsLen := len(nums)
-	l := make([]int, numsLen)
-	r := make([]int, numsLen)
 	result := make([]int, numsLen)
-	l[0] = 1
+	result[0] = 1
 	for i := 1; i < numsLen; i++ {
-		l[i] = l[i-1] * nums[i-1]
+		result[i] = result[i-1] * nums[i-1]
 	}
-	r[numsLen-1] = 1
-	for i := numsLen - 2; i >= 0; i-- {
-		r[i] = r[i+1] * nums[i+1]
-	}
-	for i := 0; i < numsLen; i++ {
-		result[i] = l[i] * r[i]
+	r := 1
+	for i := numsLen - 1; i >= 0; i-- {
+		result[i] = result[i] * r
+		r *= nums[i]
 	}
 	return result
 }
