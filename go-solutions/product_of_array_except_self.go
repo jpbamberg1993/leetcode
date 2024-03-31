cc71dcdb100c228cd5219d65ac4cea1f53674180
Paul Bamberg <jpbamberg1993@gmail.com>
2024-03-30 21:34:31 -0400

    238. Product of Array Except Self - Go - Reduce number of if statements and method calls

--- go-solutions/product_of_array_except_self.go
+++ go-solutions/product_of_array_except_self.go
@@ -1,24 +1,19 @@
 package leetcode
 
 func ProductExceptSelf(nums []int) []int {
-	l := make([]int, len(nums))
-	r := make([]int, len(nums))
-	for i := 0; i < len(nums); i++ {
-		if i == 0 {
-			l[i] = 1
-		} else {
-			l[i] = l[i-1] * nums[i-1]
-		}
+	numsLen := len(nums)
+	l := make([]int, numsLen)
+	r := make([]int, numsLen)
+	result := make([]int, numsLen)
+	l[0] = 1
+	for i := 1; i < numsLen; i++ {
+		l[i] = l[i-1] * nums[i-1]
 	}
-	for i := len(r) - 1; i >= 0; i-- {
-		if i == len(r)-1 {
-			r[i] = 1
-		} else {
-			r[i] = r[i+1] * nums[i+1]
-		}
+	r[numsLen-1] = 1
+	for i := numsLen - 2; i >= 0; i-- {
+		r[i] = r[i+1] * nums[i+1]
 	}
-	result := make([]int, len(nums))
-	for i := 0; i < len(result); i++ {
+	for i := 0; i < numsLen; i++ {
 		result[i] = l[i] * r[i]
 	}
 	return result
