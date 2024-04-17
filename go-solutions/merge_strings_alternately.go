f60827ccb9eda8451ac679cabf3d7a4a44db3982
Paul Bamberg <jpbamberg1993@gmail.com>
2024-04-17 19:12:44 -0400

    1768. Merge Strings Alternately - Go - Solve time = O(m+n), space = O(1)

--- go-solutions/merge_strings_alternately.go
+++ go-solutions/merge_strings_alternately.go
@@ -3,18 +3,16 @@ package leetcode
 import "strings"
 
 func MergeAlternately(word1 string, word2 string) string {
-	runes1 := []rune(word1)
-	runes2 := []rune(word2)
-	lenRunes1 := len(runes1)
-	lenRunes2 := len(runes2)
-	var responseString strings.Builder
-	for i := 0; i < max(lenRunes1, lenRunes2); i++ {
-		if i < lenRunes1 {
-			responseString.WriteRune(runes1[i])
+	m := len(word1)
+	n := len(word2)
+	s := strings.Builder{}
+	for i := 0; i < max(m, n); i++ {
+		if i < len(word1) {
+			s.WriteByte(word1[i])
 		}
-		if i < lenRunes2 {
-			responseString.WriteRune(runes2[i])
+		if i < len(word2) {
+			s.WriteByte(word2[i])
 		}
 	}
-	return responseString.String()
+	return s.String()
 }
