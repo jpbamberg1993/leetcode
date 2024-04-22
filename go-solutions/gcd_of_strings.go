8c2bbca1496583b922be1c04bcaf82e1c31a847c
Paul Bamberg <jpbamberg1993@gmail.com>
2024-04-22 08:41:51 -0400

    Greatest common denominator - Go - Solve using the Euclidean algorithm

--- go-solutions/gcd_of_strings.go
+++ go-solutions/gcd_of_strings.go
@@ -3,13 +3,12 @@ package leetcode
 func GcdOfString(str1 string, str2 string) string {
 	if str1+str2 != str2+str1 {
 		return ""
-	} else {
-		gcdLength := gcd(len(str1), len(str2))
-		return str1[0:gcdLength]
 	}
+	gcdLen := gcd(len(str1), len(str2))
+	return str1[:gcdLen]
 }
 
-func gcd(x, y int) int {
+func gcd(x int, y int) int {
 	if y == 0 {
 		return x
 	} else {
