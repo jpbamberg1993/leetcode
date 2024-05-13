11401dca85f6b214bd58c1f991cd730c54323a03
Paul Bamberg <jpbamberg1993@gmail.com>
2024-05-13 19:15:02 -0400

    238. Product of Array Except Self - Solve again creating one array and two loops

--- go-solutions/product_of_array_except_self.go
+++ go-solutions/product_of_array_except_self.go
@@ -1,16 +1,16 @@
 package leetcode
 
 func ProductExceptSelf(nums []int) []int {
-	numsLen := len(nums)
-	result := make([]int, numsLen)
-	result[0] = 1
-	for i := 1; i < numsLen; i++ {
-		result[i] = result[i-1] * nums[i-1]
+	length := len(nums)
+	answer := make([]int, length)
+	answer[0] = 1
+	for i := 1; i < length; i++ {
+		answer[i] = answer[i-1] * nums[i-1]
 	}
 	r := 1
-	for i := numsLen - 1; i >= 0; i-- {
-		result[i] = result[i] * r
+	for i := length - 1; i >= 0; i-- {
+		answer[i] *= r
 		r *= nums[i]
 	}
-	return result
+	return answer
 }
--- ts-solutions/src/238-product-except-self.ts
+++ ts-solutions/src/238-product-except-self.ts
@@ -1,13 +1,13 @@
 export function productExceptSelf(nums: number[]): number[] {
-	const numsLen = nums.length
-	const answer = new Array(numsLen).fill(0)
+	const length = nums.length
+	const answer = new Array<number>(length).fill(0)
 	answer[0] = 1
-	for (let i = 1; i < numsLen; i++) {
+	for (let i = 1; i < length; i++) {
 		answer[i] = answer[i - 1] * nums[i - 1]
 	}
 	let r = 1
-	for (let i = numsLen - 1; i >= 0; i--) {
-		answer[i] = answer[i] * r
+	for (let i = length - 1; i >= 0; i--) {
+		answer[i] *= r
 		r *= nums[i]
 	}
 	return answer
