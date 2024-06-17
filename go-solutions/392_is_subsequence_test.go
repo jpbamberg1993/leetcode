786c0fe3dee528b20be8e004f93dd73d58875cf1
Paul Bamberg <jpbamberg1993@gmail.com>
2024-06-17 08:48:36 -0400

    Is Subsequence solve again if t is contant

--- go-solutions/392_is_subsequence_test.go
+++ go-solutions/392_is_subsequence_test.go
@@ -3,21 +3,42 @@ package leetcode
 import "testing"
 
 type IsSubsequenceTest struct {
+	t       string
+	sToTest []SToTest
+}
+
+type SToTest struct {
 	s        string
-	t        string
 	expected bool
 }
 
-var isSubsequenceTests = []IsSubsequenceTest{
-	{s: "abc", t: "ahbgdc", expected: true},
-	{s: "axc", t: "ahbgdc", expected: false},
+//func TestIsSubsequence(t *testing.T) {
+//	for _, test := range isSubsequenceTests {
+//		got := IsSubsequence(test.s, test.t)
+//		if got != test.expected {
+//			t.Errorf("passed %s and %s returned %t expected %t", test.s, test.t, got, test.expected)
+//		}
+//	}
+//}
+
+func TestBaseOne(t *testing.T) {
+	base := NewBase("ahbgdc")
+	tests := []SToTest{
+		{s: "abc", expected: true},
+		{s: "axc", expected: false},
+	}
+	for _, test := range tests {
+		if result := base.IsSubsequence(test.s); result != test.expected {
+			t.Errorf("passed %v want %v got %v", test.s, test.expected, result)
+		}
+	}
 }
 
-func TestIsSubsequence(t *testing.T) {
-	for _, test := range isSubsequenceTests {
-		got := IsSubsequence(test.s, test.t)
-		if got != test.expected {
-			t.Errorf("passed %s and %s returned %t expected %t", test.s, test.t, got, test.expected)
-		}
+func BenchmarkIsSubsequence(b *testing.B) {
+	base := NewBase("ahbgdc")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		base.IsSubsequence("abc")
+		base.IsSubsequence("axc")
 	}
 }
