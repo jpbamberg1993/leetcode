aee26acf8cc725a3e7fa7266067361c4948b078d
Paul Bamberg <jpbamberg1993@gmail.com>
2024-08-27 17:14:52 -0400

    1657. Close Strings - Clean up hashmap solution

--- go-solutions/1657_close_strings.go
+++ go-solutions/1657_close_strings.go
@@ -1,55 +1,44 @@
 package leetcode
 
-import "sort"
-
-type kv struct {
-	key   byte
-	value int
-}
+import (
+	"reflect"
+	"sort"
+)
 
 func CloseStrings(word1 string, word2 string) bool {
 	if len(word1) != len(word2) {
 		return false
 	}
-
 	word1Map := map[byte]int{}
 	word2Map := map[byte]int{}
 	for i := 0; i < len(word1); i++ {
 		word1Map[word1[i]]++
 		word2Map[word2[i]]++
 	}
+	if !mapsHaveEqualKeys(word1Map, word2Map) {
+		return false
+	}
+	word1Frequency := mapValuesToSortedList(word1Map)
+	word2Frequency := mapValuesToSortedList(word2Map)
+	return reflect.DeepEqual(word1Frequency, word2Frequency)
+}
 
-	if len(word1Map) != len(word2Map) {
+func mapValuesToSortedList(m map[byte]int) []int {
+	values := make([]int, 0, len(m))
+	for _, v := range m {
+		values = append(values, v)
+	}
+	sort.Ints(values)
+	return values
+}
+
+func mapsHaveEqualKeys[K comparable, V1, V2 any](map1 map[K]V1, map2 map[K]V2) bool {
+	if len(map1) != len(map2) {
 		return false
 	}
 
-	for v, _ := range word1Map {
-		if _, ok := word2Map[v]; !ok {
-			return false
-		}
-	}
-
-	var word1KV []kv
-	for k, v := range word1Map {
-		word1KV = append(word1KV, kv{k, v})
-	}
-	var word2KV []kv
-	for k, v := range word2Map {
-		word2KV = append(word2KV, kv{k, v})
-	}
-
-	sort.Slice(word1KV, func(i, j int) bool {
-		return word1KV[i].value < word1KV[j].value
-	})
-
-	sort.Slice(word2KV, func(i, j int) bool {
-		return word2KV[i].value < word2KV[j].value
-	})
-
-	for i := 0; i < len(word1KV); i++ {
-		word1V := word1KV[i].value
-		word2V := word2KV[i].value
-		if word1V != word2V {
+	for key := range map1 {
+		if _, ok := map2[key]; !ok {
 			return false
 		}
 	}
