537a4084f3fee7ae57cb1cd7f02e0637c6418417
Paul Bamberg <jpbamberg1993@gmail.com>
2024-08-28 17:29:25 -0400

    1657. Close Strings - Solve using hash map and bitwise operation

--- go-solutions/1657_close_strings.go
+++ go-solutions/1657_close_strings.go
@@ -11,17 +11,19 @@ func CloseStrings(word1 string, word2 string) bool {
 	}
 	word1Map := [26]int{}
 	word2Map := [26]int{}
+	word1Bit := 0
+	word2Bit := 0
 	for i := 0; i < len(word1); i++ {
-		word1Map[word1[i]-'a']++
-		word2Map[word2[i]-'a']++
+		char1ASC := word1[i] - 'a'
+		word1Map[char1ASC]++
+		word1Bit = word1Bit | (1 << (char1ASC))
+
+		char2ASC := word2[i] - 'a'
+		word2Map[char2ASC]++
+		word2Bit = word2Bit | (1 << (char2ASC))
 	}
-	for i := 0; i < len(word1Map); i++ {
-		if word1Map[i] > 0 && word2Map[i] == 0 {
-			return false
-		}
-		if word1Map[i] == 0 && word2Map[i] > 0 {
-			return false
-		}
+	if word1Bit != word2Bit {
+		return false
 	}
 	sort.Ints(word1Map[:])
 	sort.Ints(word2Map[:])
