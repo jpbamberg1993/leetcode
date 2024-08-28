b859fefcede255796e3140a7463b6c669c495cbe
Paul Bamberg <jpbamberg1993@gmail.com>
2024-08-28 07:44:48 -0400

    1657. Close Strings - Solve again using a frequency map

--- go-solutions/1657_close_strings.go
+++ go-solutions/1657_close_strings.go
@@ -9,39 +9,21 @@ func CloseStrings(word1 string, word2 string) bool {
 	if len(word1) != len(word2) {
 		return false
 	}
-	word1Map := map[byte]int{}
-	word2Map := map[byte]int{}
+	word1Map := [26]int{}
+	word2Map := [26]int{}
 	for i := 0; i < len(word1); i++ {
-		word1Map[word1[i]]++
-		word2Map[word2[i]]++
+		word1Map[word1[i]-'a']++
+		word2Map[word2[i]-'a']++
 	}
-	if !mapsHaveEqualKeys(word1Map, word2Map) {
-		return false
-	}
-	word1Frequency := mapValuesToSortedList(word1Map)
-	word2Frequency := mapValuesToSortedList(word2Map)
-	return reflect.DeepEqual(word1Frequency, word2Frequency)
-}
-
-func mapValuesToSortedList(m map[byte]int) []int {
-	values := make([]int, 0, len(m))
-	for _, v := range m {
-		values = append(values, v)
-	}
-	sort.Ints(values)
-	return values
-}
-
-func mapsHaveEqualKeys[K comparable, V1, V2 any](map1 map[K]V1, map2 map[K]V2) bool {
-	if len(map1) != len(map2) {
-		return false
-	}
-
-	for key := range map1 {
-		if _, ok := map2[key]; !ok {
+	for i := 0; i < len(word1Map); i++ {
+		if word1Map[i] > 0 && word2Map[i] == 0 {
+			return false
+		}
+		if word1Map[i] == 0 && word2Map[i] > 0 {
 			return false
 		}
 	}
-
-	return true
+	sort.Ints(word1Map[:])
+	sort.Ints(word2Map[:])
+	return reflect.DeepEqual(word1Map, word2Map)
 }
