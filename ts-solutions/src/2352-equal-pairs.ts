2d19600a37ffe410e8d6e1973c3d7d376d899921
Paul Bamberg <jpbamberg1993@gmail.com>
2024-09-30 07:51:47 -0400

    2352 Equal Pairs - TS solve using a hasmap which was faster than trie

--- ts-solutions/src/2352-equal-pairs.ts
+++ ts-solutions/src/2352-equal-pairs.ts
@@ -1,58 +1,27 @@
 export const equalPairs = (grid: number[][]): number => {
-	const node = new Trie()
-	for (const arr of grid) {
-		node.setNodes(arr)
+	const rows = new Map<string, number>()
+	for (const row of grid) {
+		const key = numsToString(row)
+		if (rows.has(key)) {
+			rows.set(key, rows.get(key)! + 1)
+		} else {
+			rows.set(key, 1)
+		}
 	}
 	let count = 0
 	for (let i = 0; i < grid.length; i++) {
-		const column = []
+		const column = new Array<number>()
 		for (let j = 0; j < grid.length; j++) {
 			column[j] = grid[j][i]
 		}
-		count += node.searchNodes(column)
+		const key = numsToString(column)
+		if (rows.has(key)) {
+			count += rows.get(key)!
+		}
 	}
 	return count
 }
 
-class TrieNode {
-	public count: number
-	public children: Map<number, TrieNode>
-
-	constructor() {
-		this.count = 0
-		this.children = new Map<number, TrieNode>()
-	}
-}
-
-class Trie {
-	public root: TrieNode
-
-	constructor() {
-		this.root = new TrieNode()
-	}
-
-	setNodes = (arr: number[]) => {
-		let myNode = this.root
-		for (const n of arr) {
-			const node = myNode.children.get(n)
-			if (!node) {
-				myNode.children.set(n, new TrieNode())
-			}
-			myNode = myNode.children.get(n)!
-		}
-		myNode.count++
-	}
-
-	searchNodes = (arr: number[]) => {
-		let myTrie = this.root
-		for (const n of arr) {
-			const node = myTrie.children.get(n)
-			if (node) {
-				myTrie = node
-			} else {
-				return 0
-			}
-		}
-		return myTrie.count
-	}
+const numsToString = (nums: number[]) => {
+	return nums.join(`,`)
 }
