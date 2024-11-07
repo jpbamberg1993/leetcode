929b276536191983a3ac2ce9f4daf34576fffdb5
Paul Bamberg <jpbamberg1993@gmail.com>
2024-11-07 08:29:47 -0500

    394 Decode String - Solve using sigle for loop

--- go-solutions/394_decode_string.go
+++ go-solutions/394_decode_string.go
@@ -1,36 +1,32 @@
 package leetcode
 
-import "unicode"
+type item struct {
+	n     int
+	bytes []byte
+}
 
 func DecodeString(s string) string {
-	stack := make([]rune, 0)
-	for i := 0; i < len(s); i++ {
-		ch := rune(s[i])
-		if ch != ']' {
-			stack = append(stack, ch)
-		} else {
-			var temp []rune
-			for stack[len(stack)-1] != '[' {
-				lastChar := stack[len(stack)-1]
-				stack = stack[:len(stack)-1]
-				temp = append(temp, lastChar)
+	num := 0
+	st := []item{{0, []byte{}}}
+
+	for _, ch := range s {
+		switch {
+		case ch == '0':
+			num *= 10
+		case ch > '0' && ch <= '9':
+			num = num*10 + int(ch-'0')
+		case ch == '[':
+			st = append(st, item{num, []byte{}})
+			num = 0
+		case ch == ']':
+			tmp := st[len(st)-1]
+			st = st[:len(st)-1]
+			for i := 0; i < tmp.n; i++ {
+				st[len(st)-1].bytes = append(st[len(st)-1].bytes, tmp.bytes...)
 			}
-			stack = stack[:len(stack)-1]
-			base := 1
-			k := 0
-			for len(stack) > 0 && unicode.IsDigit(stack[len(stack)-1]) {
-				lastChar := stack[len(stack)-1]
-				stack = stack[:len(stack)-1]
-				k = k + int(lastChar-'0')*base
-				base *= 10
-			}
-			for k != 0 {
-				for i := len(temp) - 1; i >= 0; i-- {
-					stack = append(stack, temp[i])
-				}
-				k--
-			}
+		default:
+			st[len(st)-1].bytes = append(st[len(st)-1].bytes, byte(ch))
 		}
 	}
-	return string(stack)
+	return string(st[len(st)-1].bytes)
 }
