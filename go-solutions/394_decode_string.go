d61ad5295ab444312c6f5512d9f9eb82d29ea58e
Paul Bamberg <jpbamberg1993@gmail.com>
2024-11-26 18:15:59 -0500

    clean solution with one loop

--- go-solutions/394_decode_string.go
+++ go-solutions/394_decode_string.go
@@ -1,32 +1,32 @@
 package leetcode
 
-type item struct {
-	n     int
-	bytes []byte
-}
-
 func DecodeString(s string) string {
-	num := 0
 	st := []item{{0, []byte{}}}
+	num := 0
 
-	for _, ch := range s {
+	for _, c := range s {
 		switch {
-		case ch == '0':
+		case c > '0' && c <= '9':
+			num = num*10 + int(c-'0')
+		case c == '0':
 			num *= 10
-		case ch > '0' && ch <= '9':
-			num = num*10 + int(ch-'0')
-		case ch == '[':
+		case c == '[':
 			st = append(st, item{num, []byte{}})
 			num = 0
-		case ch == ']':
+		case c == ']':
 			tmp := st[len(st)-1]
 			st = st[:len(st)-1]
-			for i := 0; i < tmp.n; i++ {
+			for i := 0; i < tmp.num; i++ {
 				st[len(st)-1].bytes = append(st[len(st)-1].bytes, tmp.bytes...)
 			}
 		default:
-			st[len(st)-1].bytes = append(st[len(st)-1].bytes, byte(ch))
+			st[len(st)-1].bytes = append(st[len(st)-1].bytes, byte(c))
 		}
 	}
 	return string(st[len(st)-1].bytes)
+}
+
+type item struct {
+	num   int
+	bytes []byte
 }
--- go-solutions/394_decode_string_test.go
+++ go-solutions/394_decode_string_test.go
@@ -11,6 +11,7 @@ var decodeStringTests = []decodeStringTest{
 	{s: "3[a]2[bc]", want: "aaabcbc"},
 	{s: "3[a2[c]]", want: "accaccacc"},
 	{s: "2[abc]3[cd]ef", want: "abcabccdcdcdef"},
+	{s: "2[20[bc]31[xy]]xd4[rt]", want: "bcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxybcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxyxdrtrtrtrt"},
 }
 
 func TestDecodeString(t *testing.T) {
