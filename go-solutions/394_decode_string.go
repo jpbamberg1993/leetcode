4b5fa73cecaac4f66985d9f122a29cf26dfb7711
Paul Bamberg <jpbamberg1993@gmail.com>
2025-01-02 17:41:34 -0500

    394. Decode String - update to use recursion

--- go-solutions/394_decode_string.go
+++ go-solutions/394_decode_string.go
@@ -1,36 +1,38 @@
 package leetcode
 
+import "strings"
+
+var index int
+
 func DecodeString(s string) string {
-	var stack []int32
-	for _, ch := range s {
-		if ch == ']' {
-			var decodedString []int32
+	index = 0
+	return decodeString(s)
+}
 
-			for len(stack) > 0 && stack[len(stack)-1] != '[' {
-				decodedString = append(decodedString, stack[len(stack)-1])
-				stack = stack[:len(stack)-1]
-			}
-
-			stack = stack[:len(stack)-1]
-
-			var k int32
-			var base int32
-			base = 1
-			for len(stack) > 0 && stack[len(stack)-1] <= '9' && stack[len(stack)-1] >= '0' {
-				k = k + ((stack[len(stack)-1] - '0') * base)
-				base *= 10
-				stack = stack[:len(stack)-1]
-			}
-
-			for k > 0 {
-				for i := len(decodedString) - 1; i >= 0; i-- {
-					stack = append(stack, decodedString[i])
-				}
-				k--
-			}
+func decodeString(s string) string {
+	result := strings.Builder{}
+	for index < len(s) && s[index] != ']' {
+		if !isDigit(s) {
+			result.WriteByte(s[index])
+			index++
 		} else {
-			stack = append(stack, ch)
+			n := 0
+			for isDigit(s) {
+				n = n*10 + int(s[index]-'0')
+				index++
+			}
+			index++
+			decodedString := decodeString(s)
+			index++
+			for n > 0 {
+				result.WriteString(decodedString)
+				n--
+			}
 		}
 	}
-	return string(stack[:])
+	return result.String()
+}
+
+func isDigit(s string) bool {
+	return s[index] >= '0' && s[index] <= '9'
 }
