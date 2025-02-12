ec6041e651ff1f96d8890f6e6604dc87130a76da
Paul Bamberg <jpbamberg1993@gmail.com>
2025-02-12 07:49:22 -0500

    2352. Equal Pairs - Solve using map and JSON.stringify

--- ts-solutions/src/2352-equal-pairs.ts
+++ ts-solutions/src/2352-equal-pairs.ts
@@ -1,57 +1,13 @@
-class TrieNode {
-	children: Map<number, TrieNode>
-	count: number
-
-	constructor() {
-		this.children = new Map()
-		this.count = 0
+export const equalPairs = (grid: number[][]) => {
+	const rows = new Map<string, number>()
+	for (let i = 0; i < grid.length; i++) {
+		const row = JSON.stringify(grid[i])
+		rows.set(row, 1 + (rows.get(row) || 0))
 	}
-}
-
-class Trie {
-	root: TrieNode
-
-	constructor() {
-		this.root = new TrieNode()
-	}
-
-	insert(row: number[]) {
-		let currentTrie = this.root
-		for (const num of row) {
-			if (!currentTrie.children.has(num)) {
-				currentTrie.children.set(num, new TrieNode())
-			}
-			currentTrie = currentTrie.children.get(num) ?? new TrieNode()
-		}
-		currentTrie.count++
-	}
-
-	search(column: number[]) {
-		let currentTrie = this.root
-		for (const n of column) {
-			currentTrie = currentTrie.children.get(n) ?? new TrieNode()
-			if (!currentTrie) {
-				return 0
-			}
-		}
-		return currentTrie.count
-	}
-}
-
-export const equalPairs = (nums: number[][]) => {
-	const trie = new Trie()
-	for (const row of nums) {
-		trie.insert(row)
-	}
-
 	let count = 0
-	for (let i = 0; i < nums.length; i++) {
-		const rowLength = nums[i].length
-		const column = new Array<number>(rowLength).fill(0)
-		for (let j = 0; j < rowLength; j++) {
-			column[j] = nums[j][i]
-		}
-		count += trie.search(column)
+	for (let i = 0; i < grid.length; i++) {
+		const column = JSON.stringify(grid.map((g) => g[i]))
+		count += rows.get(column) || 0
 	}
 	return count
 }
