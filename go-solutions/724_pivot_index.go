68fe13cca020611e6772bae68c5aa9514347dc73
Paul Bamberg <jpbamberg1993@gmail.com>
2025-03-20 08:30:26 -0400

    724. Pivot Index - Detmine pivot index by adding and subtracting

--- go-solutions/724_pivot_index.go
+++ go-solutions/724_pivot_index.go
@@ -1,24 +1,16 @@
 package leetcode
 
 func PivotIndex(nums []int) int {
-	leftSums := make([]int, len(nums))
-	rightSums := make([]int, len(nums))
-
-	leftSums[0] = 0
-	for i := 1; i < len(nums); i++ {
-		leftSums[i] = leftSums[i-1] + nums[i-1]
+	sum := 0
+	leftSum := 0
+	for _, v := range nums {
+		sum += v
 	}
-
-	rightSums[len(nums)-1] = 0
-	for i := len(nums) - 2; i >= 0; i-- {
-		rightSums[i] = rightSums[i+1] + nums[i+1]
-	}
-
-	for i := 0; i < len(nums); i++ {
-		if leftSums[i] == rightSums[i] {
+	for i, v := range nums {
+		if leftSum == sum-leftSum-v {
 			return i
 		}
+		leftSum += v
 	}
-
 	return -1
 }
