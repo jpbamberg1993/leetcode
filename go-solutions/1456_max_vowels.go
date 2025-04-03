74207f0b39269679e81968efa81c1b0570148f08
Paul Bamberg <jpbamberg1993@gmail.com>
2025-04-03 17:29:39 -0400

    1456 Max Vowel - Solve again with O(n)

--- go-solutions/1456_max_vowels.go
+++ go-solutions/1456_max_vowels.go
@@ -4,24 +4,24 @@ import (
 	"slices"
 )
 
-var vowels = []uint8{97, 101, 105, 111, 117}
+var vowels = []uint8{'a', 'e', 'i', 'o', 'u'}
 
 func MaxVowels(s string, k int) int {
-	maxValue := 0
+	currentMax := 0
 	for i := 0; i < k; i++ {
 		if slices.Contains(vowels, s[i]) {
-			maxValue++
+			currentMax++
 		}
 	}
-	currentCount := maxValue
+	result := currentMax
 	for i := k; i < len(s); i++ {
 		if slices.Contains(vowels, s[i]) {
-			currentCount++
+			currentMax++
 		}
 		if slices.Contains(vowels, s[i-k]) {
-			currentCount--
+			currentMax--
 		}
-		maxValue = max(maxValue, currentCount)
+		result = max(result, currentMax)
 	}
-	return maxValue
+	return result
 }
