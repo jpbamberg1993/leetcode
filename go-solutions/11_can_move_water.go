fa15ed1f9f98158f005ed70b01351b12981cb52f
Paul Bamberg <jpbamberg1993@gmail.com>
2025-04-28 09:12:22 -0400

    11. Container with most water - Solve again using 2 pointers

--- go-solutions/11_can_move_water.go
+++ go-solutions/11_can_move_water.go
@@ -5,7 +5,7 @@ import "go-solutions/utils"
 func MaxArea(height []int) int {
 	left, right, maxArea := 0, len(height)-1, 0
 	for left < right {
-		area := utils.Abs(right-left) * utils.Min(height[left], height[right])
+		area := (right - left) * utils.Min(height[left], height[right])
 		maxArea = max(maxArea, area)
 		if height[left] < height[right] {
 			left++
