5189da0bcdae4455932c33ca388eff60be1a0f5c
Paul Bamberg <jpbamberg1993@gmail.com>
2025-05-06 18:50:12 -0400

    392. Is Subsequence - Greedy match with hashmap

--- go-solutions/392_is_subsequence.go
+++ go-solutions/392_is_subsequence.go
@@ -1,16 +1,50 @@
 package leetcode
 
-func IsSubsequence(s, t string) bool {
-	if len(s) > len(t) {
-		return true
+//func IsSubsequence(s, t string) bool {
+//	if len(s) > len(t) {
+//		return true
+//	}
+//	left := 0
+//	right := 0
+//	for left < len(s) && right < len(t) {
+//		if s[left] == t[right] {
+//			left++
+//		}
+//		right++
+//	}
+//	return left == len(s)
+//}
+
+type Base struct {
+	tMap map[int32][]int
+}
+
+func NewBase(t string) *Base {
+	tMap := make(map[int32][]int, len(t))
+	for i, v := range t {
+		tMap[v] = append(tMap[v], i)
 	}
-	left := 0
-	right := 0
-	for left < len(s) && right < len(t) {
-		if s[left] == t[right] {
-			left++
+	return &Base{tMap}
+}
+
+func (b *Base) IsSubsequence(s string) bool {
+	for i, v := range s {
+		if tIndexes, exists := b.tMap[v]; exists {
+			if !containsGreater(tIndexes, i) {
+				return false
+			}
+		} else {
+			return false
 		}
-		right++
 	}
-	return left == len(s)
+	return true
+}
+
+func containsGreater(slice []int, value int) bool {
+	for _, v := range slice {
+		if v >= value {
+			return true
+		}
+	}
+	return false
 }
--- go-solutions/392_is_subsequence_test.go
+++ go-solutions/392_is_subsequence_test.go
@@ -2,49 +2,50 @@ package leetcode
 
 import "testing"
 
-type IsSubsequenceTest struct {
-	s        string
-	t        string
-	expected bool
-}
-
-var isSubsequenceTests = []IsSubsequenceTest{
-	{t: "ahbgdc", s: "abc", expected: true},
-	{t: "ahbgdc", s: "axc", expected: false},
-	{t: "ahbgdc", s: "", expected: true},
-}
-
-func TestIsSubsequence(t *testing.T) {
-	for _, test := range isSubsequenceTests {
-		got := IsSubsequence(test.s, test.t)
-		if got != test.expected {
-			t.Errorf("passed %s and %s returned %t expected %t", test.s, test.t, got, test.expected)
-		}
-	}
-}
-
 //type IsSubsequenceTest struct {
 //	t       string
 //	sToTest []SToTest
 //}
-//
-//type SToTest struct {
+
+//type IsSubsequenceTest struct {
 //	s        string
+//	t        string
 //	expected bool
 //}
-
-//func TestBaseOne(t *testing.T) {
-//	base := NewBase("ahbgdc")
-//	tests := []SToTest{
-//		{s: "abc", expected: true},
-//		{s: "axc", expected: false},
-//	}
-//	for _, test := range tests {
-//		if result := base.IsSubsequence(test.s); result != test.expected {
-//			t.Errorf("passed %v want %v got %v", test.s, test.expected, result)
+//
+//var isSubsequenceTests = []IsSubsequenceTest{
+//	{t: "ahbgdc", s: "abc", expected: true},
+//	{t: "ahbgdc", s: "axc", expected: false},
+//	{t: "ahbgdc", s: "", expected: true},
+//}
+//
+//func TestIsSubsequence(t *testing.T) {
+//	for _, test := range isSubsequenceTests {
+//		got := IsSubsequence(test.s, test.t)
+//		if got != test.expected {
+//			t.Errorf("passed %s and %s returned %t expected %t", test.s, test.t, got, test.expected)
 //		}
 //	}
 //}
+
+type SToTest struct {
+	s        string
+	expected bool
+}
+
+func TestBaseOne(t *testing.T) {
+	base := NewBase("ahbgdc")
+	tests := []SToTest{
+		{s: "abc", expected: true},
+		{s: "axc", expected: false},
+		{s: "gdc", expected: true},
+	}
+	for _, test := range tests {
+		if result := base.IsSubsequence(test.s); result != test.expected {
+			t.Errorf("passed %v want %v got %v", test.s, test.expected, result)
+		}
+	}
+}
 
 //func BenchmarkIsSubsequence(b *testing.B) {
 //	base := NewBase("ahbgdc")
