e0e8496feeb3ba8e3e7bec6469ddb1a17931e273
Paul Bamberg <jpbamberg1993@gmail.com>
2025-05-12 07:59:33 -0400

    392. Is Subsequence - Solve using dp

--- go-solutions/392_is_subsequence.go
+++ go-solutions/392_is_subsequence.go
@@ -1,50 +1,54 @@
 package leetcode
 
-//func IsSubsequence(s, t string) bool {
-//	if len(s) > len(t) {
-//		return true
-//	}
-//	left := 0
-//	right := 0
-//	for left < len(s) && right < len(t) {
-//		if s[left] == t[right] {
-//			left++
-//		}
-//		right++
-//	}
-//	return left == len(s)
-//}
-
-type Base struct {
-	tMap map[int32][]int
-}
-
-func NewBase(t string) *Base {
-	tMap := make(map[int32][]int, len(t))
-	for i, v := range t {
-		tMap[v] = append(tMap[v], i)
+func IsSubsequence(s, t string) bool {
+	dp := make([][]int, len(t)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(s)+1)
 	}
-	return &Base{tMap}
-}
 
-func (b *Base) IsSubsequence(s string) bool {
-	for i, v := range s {
-		if tIndexes, exists := b.tMap[v]; exists {
-			if !containsGreater(tIndexes, i) {
-				return false
+	for i := 1; i < len(t)+1; i++ {
+		for j := 1; j < len(s)+1; j++ {
+			if s[j-1] == t[i-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 			}
-		} else {
-			return false
 		}
 	}
-	return true
+
+	return dp[len(t)][len(s)] == len(s)
 }
 
-func containsGreater(slice []int, value int) bool {
-	for _, v := range slice {
-		if v >= value {
-			return true
-		}
-	}
-	return false
-}
+//type Base struct {
+//	tMap map[int32][]int
+//}
+//
+//func NewBase(t string) *Base {
+//	tMap := make(map[int32][]int, len(t))
+//	for i, v := range t {
+//		tMap[v] = append(tMap[v], i)
+//	}
+//	return &Base{tMap}
+//}
+//
+//func (b *Base) IsSubsequence(s string) bool {
+//	for i, v := range s {
+//		if tIndexes, exists := b.tMap[v]; exists {
+//			if !containsGreater(tIndexes, i) {
+//				return false
+//			}
+//		} else {
+//			return false
+//		}
+//	}
+//	return true
+//}
+//
+//func containsGreater(slice []int, value int) bool {
+//	for _, v := range slice {
+//		if v >= value {
+//			return true
+//		}
+//	}
+//	return false
+//}
