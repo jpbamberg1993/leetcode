55f7697200d80bd7f46369dbf18b9f5ec9b41266
Paul Bamberg <jpbamberg1993@gmail.com>
2025-07-29 08:14:24 -0400

    283. Move Zeroes - Keep index of last time a non zero number was set

--- go-solutions/283_move_zeroes.go
+++ go-solutions/283_move_zeroes.go
@@ -1,11 +1,15 @@
 package leetcode
 
-func MoveZeroes(nums []int) {
-	lastNonZero := 0
+func MoveZeroes(nums []int) []int {
+	lastNoneZero := 0
 	for i := 0; i < len(nums); i++ {
 		if nums[i] != 0 {
-			nums[lastNonZero], nums[i] = nums[i], nums[lastNonZero]
-			lastNonZero++
+			nums[lastNoneZero] = nums[i]
+			lastNoneZero++
 		}
 	}
+	for i := lastNoneZero; i < len(nums); i++ {
+		nums[i] = 0
+	}
+	return nums
 }
