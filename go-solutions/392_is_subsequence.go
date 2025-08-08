8256d188c3ef3d46bc3976c5ec83b346f1afb0e2
Paul Bamberg <jpbamberg1993@gmail.com>
2025-08-08 19:09:15 -0400

    392. Is Subsequence - solve using dynamic programming

--- go-solutions/392_is_subsequence.go
+++ go-solutions/392_is_subsequence.go
@@ -1,54 +1,25 @@
 package leetcode
 
 func IsSubsequence(s, t string) bool {
-	dp := make([][]int, len(t)+1)
-	for i := range dp {
-		dp[i] = make([]int, len(s)+1)
+	rowLen, colLen := len(s)+1, len(t)+1
+	if rowLen == 1 {
+		return true
 	}
-
-	for i := 1; i < len(t)+1; i++ {
-		for j := 1; j < len(s)+1; j++ {
-			if s[j-1] == t[i-1] {
-				dp[i][j] = dp[i-1][j-1] + 1
+	dp := make([][]int, rowLen)
+	for i := 0; i < rowLen; i++ {
+		dp[i] = make([]int, colLen)
+	}
+	for col := 1; col < colLen; col++ {
+		for row := 1; row < rowLen; row++ {
+			if t[col-1] == s[row-1] {
+				dp[row][col] = dp[row-1][col-1] + 1
 			} else {
-				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+				dp[row][col] = max(dp[row-1][col], dp[row][col-1])
 			}
 		}
+		if dp[rowLen-1][col] == rowLen-1 {
+			return true
+		}
 	}
-
-	return dp[len(t)][len(s)] == len(s)
+	return false
 }
-
-//type Base struct {
-//	tMap map[int32][]int
-//}
-//
-//func NewBase(t string) *Base {
-//	tMap := make(map[int32][]int, len(t))
-//	for i, v := range t {
-//		tMap[v] = append(tMap[v], i)
-//	}
-//	return &Base{tMap}
-//}
-//
-//func (b *Base) IsSubsequence(s string) bool {
-//	for i, v := range s {
-//		if tIndexes, exists := b.tMap[v]; exists {
-//			if !containsGreater(tIndexes, i) {
-//				return false
-//			}
-//		} else {
-//			return false
-//		}
-//	}
-//	return true
-//}
-//
-//func containsGreater(slice []int, value int) bool {
-//	for _, v := range slice {
-//		if v >= value {
-//			return true
-//		}
-//	}
-//	return false
-//}
