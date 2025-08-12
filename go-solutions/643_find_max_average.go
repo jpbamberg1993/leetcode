30abfd9fdd75dd6fc80d6867cdf1e9b0f51339bc
Paul Bamberg <jpbamberg1993@gmail.com>
2025-08-11 21:16:52 -0400

    643. Find Max Average - Solve again using one loop

--- go-solutions/643_find_max_average.go
+++ go-solutions/643_find_max_average.go
@@ -5,10 +5,10 @@ func FindMaxAverage(nums []int, k int) float64 {
 	for i := 0; i < k; i++ {
 		maxSum += nums[i]
 	}
-	currentSum := 0
+	currentSum := maxSum
 	for i := k; i < len(nums); i++ {
 		currentSum += nums[i] - nums[i-k]
-		maxSum = max(currentSum, maxSum)
+		maxSum = max(maxSum, currentSum)
 	}
 	return float64(maxSum) / float64(k)
 }
