6bb7043ff642ae233536d89d6b03162c456f8d52
Paul Bamberg <jpbamberg1993@gmail.com>
2025-10-04 10:22:32 -0400

    2352. Equal Pairs - Solve again using trie

--- go-solutions/2352_equal_pairs.go
+++ go-solutions/2352_equal_pairs.go
@@ -1,50 +1,54 @@
 package leetcode
 
+func EqualPairs(grid [][]int) int {
+	trie := NewTrie()
+	for _, row := range grid {
+		trie.Save(row)
+	}
+	count := 0
+	for i := 0; i < len(grid); i++ {
+		column := make([]int, len(grid))
+		for j := 0; j < len(grid); j++ {
+			column[j] = grid[j][i]
+		}
+		count += trie.Search(column)
+	}
+	return count
+}
+
 type Trie struct {
-	children map[int]*Trie
-	count    int
+	next  map[int]*Trie
+	count int
 }
 
 func NewTrie() *Trie {
-	return &Trie{children: make(map[int]*Trie), count: 0}
-}
-
-func (t *Trie) Insert(column []int) {
-	currentTrie := t
-	for _, v := range column {
-		if _, ok := currentTrie.children[v]; !ok {
-			currentTrie.children[v] = NewTrie()
-		}
-		currentTrie = currentTrie.children[v]
+	return &Trie{
+		next: make(map[int]*Trie),
+		count: 0,
 	}
-	currentTrie.count++
 }
 
-func (t *Trie) Search(column []int) int {
+func (t *Trie) Save(values []int) {
 	currentTrie := t
-	for _, v := range column {
-		child, ok := currentTrie.children[v]
-		if ok {
-			currentTrie = child
+	for i := 0; i < len(values); i++ {
+		if _, ok := currentTrie.next[values[i]]; !ok {
+			currentTrie.next[values[i]] = NewTrie()
+		}
+		currentTrie = currentTrie.next[values[i]]
+		if i == len(values)-1 {
+			currentTrie.count += 1
+		}
+	}
+}
+
+func (t *Trie) Search(values []int) int {
+	currentTrie := t
+	for _, v := range values {
+		if trie, ok := currentTrie.next[v]; ok {
+			currentTrie = trie
 		} else {
 			return 0
 		}
 	}
 	return currentTrie.count
-}
-
-func EqualPairs(grid [][]int) int {
-	rootTrie := NewTrie()
-	for _, row := range grid {
-		rootTrie.Insert(row)
-	}
-	count := 0
-	for i := 0; i < len(grid); i++ {
-		column := make([]int, len(grid[i]))
-		for j := 0; j < len(grid[i]); j++ {
-			column[j] = grid[j][i]
-		}
-		count += rootTrie.Search(column)
-	}
-	return count
 }
