a1404761d816126e5e2d37bb24d9136093685759
Paul Bamberg <jpbamberg1993@gmail.com>
2025-10-23 18:46:36 -0400

    993. Number Recent Calls - solve again using container/list

--- go-solutions/993_number_recent_calls.go
+++ go-solutions/993_number_recent_calls.go
@@ -1,8 +1,6 @@
 package leetcode
 
-import (
-	"container/list"
-)
+import "container/list"
 
 type RecentCounter struct {
 	queue *list.List
@@ -12,10 +10,10 @@ func Constructor() RecentCounter {
 	return RecentCounter{queue: list.New()}
 }
 
-func (c *RecentCounter) Ping(t int) int {
-	c.queue.PushBack(t)
-	for c.queue.Front().Value.(int) < t-3000 {
-		c.queue.Remove(c.queue.Front())
+func (rc *RecentCounter) Ping(t int) int {
+	rc.queue.PushBack(t)
+	for rc.queue.Front().Value.(int) < t-3000 {
+		rc.queue.Remove(rc.queue.Front())
 	}
-	return c.queue.Len()
+	return rc.queue.Len()
 }
