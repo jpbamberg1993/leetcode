e426a297e59c312db258aa34bdd07e67af22428d
Paul Bamberg <jpbamberg1993@gmail.com>
2026-01-22 18:38:27 -0500

    1768. Merge Stringsalternately - solve again with string builder

--- go-solutions/1768_merge_strings_alternately.go
+++ go-solutions/1768_merge_strings_alternately.go
@@ -3,15 +3,15 @@ package leetcode
 import "strings"
 
 func MergeAlternately(word1, word2 string) string {
-	result := strings.Builder{}
+	s := strings.Builder{}
 	m, n := len(word1), len(word2)
 	for i := 0; i < max(m, n); i++ {
 		if i < m {
-			result.WriteByte(word1[i])
+			s.WriteByte(word1[i])
 		}
 		if i < n {
-			result.WriteByte(word2[i])
+			s.WriteByte(word2[i])
 		}
 	}
-	return result.String()
+	return s.String()
 }
