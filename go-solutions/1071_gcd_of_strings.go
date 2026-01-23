9676751e3638561a9ace47bb5a29c5427aef3243
Paul Bamberg <jpbamberg1993@gmail.com>
2026-01-23 08:21:41 -0500

    1071. GCD of Strings - solve again using the euclidian algo

--- go-solutions/1071_gcd_of_strings.go
+++ go-solutions/1071_gcd_of_strings.go
@@ -4,14 +4,13 @@ func GcdOfStrings(str1, str2 string) string {
 	if str1+str2 != str2+str1 {
 		return ""
 	}
-	gcdLength := gcd(len(str1), len(str2))
-	return str1[:gcdLength]
+	divisor := gcd(len(str1), len(str2))
+	return str1[:divisor]
 }
 
 func gcd(x, y int) int {
 	if y == 0 {
 		return x
 	}
-
 	return gcd(y, x%y)
 }
