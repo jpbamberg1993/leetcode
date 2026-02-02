b3a2aa0819109a6f74eae040aa2bed72b3dd9d8e
Paul Bamberg <jpbamberg1993@gmail.com>
2026-02-02 09:37:04 -0500

    11. Can Move Water - solve again using two pointers

--- go-solutions/11_can_move_water.go
+++ go-solutions/11_can_move_water.go
@@ -1,15 +1,15 @@
 package leetcode
 
 func MaxArea(height []int) int {
-	maxArea := 0
+	maxArea := 1
 	left, right := 0, len(height)-1
 	for left < right {
-		area := (right - left) * min(height[left], height[right])
-		maxArea = max(maxArea, area)
-		if height[left] > height[right] {
-			right--
-		} else {
+		currentArea := (right - left) * min(height[left], height[right])
+		maxArea = max(currentArea, maxArea)
+		if height[left] < height[right] {
 			left++
+		} else {
+			right--
 		}
 	}
 	return maxArea
