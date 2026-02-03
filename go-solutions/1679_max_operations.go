0dad7d77510a0f6e2223864c65f7eec07ea82999
Paul Bamberg <jpbamberg1993@gmail.com>
2026-02-03 18:29:39 -0500

    1679. Max Operations - solve using 2 pointers

--- go-solutions/1679_max_operations.go
+++ go-solutions/1679_max_operations.go
@@ -1,23 +1,24 @@
 package leetcode
 
+import (
+	"sort"
+)
+
 func MaxOperations(nums []int, k int) int {
-	numsMap := make(map[int]int)
-	for _, v := range nums {
-		numsMap[v] += 1
-	}
-	count := 0
-	for _, num := range nums {
-		remainder := k - num
-		currentCount, _ := numsMap[num]
-		complimentCount, _ := numsMap[remainder]
-		if currentCount > 0 && complimentCount > 0 {
-			if num == remainder && complimentCount < 2 {
-				continue
-			}
-			numsMap[num]--
-			numsMap[complimentCount]--
-			count++
+	sort.Ints(nums)
+	result := 0
+	left, right := 0, len(nums)-1
+	for left < right {
+		sum := nums[left] + nums[right]
+		if sum == k {
+			result++
+			left++
+			right--
+		} else if sum > k {
+			right--
+		} else {
+			left++
 		}
 	}
-	return count
+	return result
 }
