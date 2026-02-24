5fde1f94550846ee6919eafce0b660621344dd9d
Paul Bamberg <jpbamberg1993@gmail.com>
2026-02-24 17:17:36 -0500

    2390. Remove Stars - solve using bytes buffer

--- go-solutions/2390_remove_stars.go
+++ go-solutions/2390_remove_stars.go
@@ -1,87 +1,19 @@
 package leetcode
 
 import (
-	"strings"
+	"bytes"
 )
 
 func RemoveStars(s string) string {
-	stack := List{}
+	var b bytes.Buffer
 	for i := 0; i < len(s); i++ {
 		if s[i] == '*' {
-			stack.PopFront()
+			if b.Len() > 0 {
+				b.Truncate(b.Len() - 1)
+			}
 		} else {
-			stack.PushFront(s[i])
+			b.WriteByte(s[i])
 		}
 	}
-	return stack.String()
+	return b.String()
 }
-
-type ListItem struct {
-	Value uint8
-	Next  *ListItem
-	Prev  *ListItem
-}
-
-type List struct {
-	head *ListItem
-	tail *ListItem
-	size int
-}
-
-func (l *List) PushFront(v uint8) {
-	n := &ListItem{Value: v}
-	if l.head == nil {
-		l.head = n
-		l.tail = n
-	} else {
-		l.head.Prev = n
-		n.Next = l.head
-		l.head = n
-	}
-	l.size++
-}
-
-func (l *List) PopFront() (v uint8) {
-	if l.size < 1 {
-		return 0
-	}
-	frontValue := l.head.Value
-	l.head = l.head.Next
-	if l.head == nil {
-		l.tail = nil
-	}
-	l.size--
-	return frontValue
-}
-
-func (l *List) String() string {
-	builder := strings.Builder{}
-	currentItem := l.tail
-	for currentItem != nil {
-		builder.WriteByte(currentItem.Value)
-		currentItem = currentItem.Prev
-	}
-	return builder.String()
-}
-
-//type Stack struct {
-//	data []byte
-//}
-//
-//func (s *Stack) Push(v byte) {
-//	s.data = append(s.data, v)
-//}
-//
-//func (s *Stack) Pop() (v byte) {
-//	lastValue := s.data[len(s.data)-1]
-//	s.data = s.data[:len(s.data)-1]
-//	return lastValue
-//}
-//
-//func (s *Stack) String() string {
-//	var b strings.Builder
-//	for _, v := range s.data {
-//		b.WriteByte(v)
-//	}
-//	return b.String()
-//}
