f7b60a6dc36efba72eb1e794f1872cb51360290c
Paul Bamberg <jpbamberg1993@gmail.com>
2026-03-06 09:02:51 -0500

    394. Decode String - solve using stack

--- go-solutions/394_decode_string.go
+++ go-solutions/394_decode_string.go
@@ -1,30 +1,28 @@
 package leetcode
 
+import "strings"
+
 func DecodeString(s string) string {
 	var st []int32
 
 	for _, v := range s {
 		if v == ']' {
-			// slice of characters in between [ and ]
-			var subSlice []int32
+			subStr := make([]int32, 0)
 			for st[len(st)-1] != '[' {
-				subSlice = append(subSlice, st[len(st)-1])
+				subStr = append(subStr, st[len(st)-1])
 				st = st[:len(st)-1]
 			}
 			st = st[:len(st)-1]
-
-			// the number of times subSlice is repeated
-			base := 1
 			k := 0
+			base := 1
 			for len(st) > 0 && st[len(st)-1] >= '0' && st[len(st)-1] <= '9' {
-				k = k + int(st[len(st)-1]-'0')*base
+				k += int(st[len(st)-1]-'0') * base
 				base *= 10
 				st = st[:len(st)-1]
 			}
-
 			for k > 0 {
-				for i := len(subSlice) - 1; i >= 0; i-- {
-					st = append(st, subSlice[i])
+				for i := len(subStr) - 1; i >= 0; i-- {
+					st = append(st, subStr[i])
 				}
 				k--
 			}
@@ -32,5 +30,10 @@ func DecodeString(s string) string {
 			st = append(st, v)
 		}
 	}
-	return string(st[:])
+
+	var result strings.Builder
+	for _, v := range st {
+		result.WriteRune(rune(v))
+	}
+	return result.String()
 }
