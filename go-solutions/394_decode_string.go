5bbb9e06dd7957bccc8c4cb77fc1e2df5410f363
Paul Bamberg <jpbamberg1993@gmail.com>
2026-03-11 07:35:48 -0400

    394. Decode String - solve again o(n)

--- go-solutions/394_decode_string.go
+++ go-solutions/394_decode_string.go
@@ -1,38 +1,39 @@
 package leetcode
 
-import "strings"
+import (
+	"strings"
+)
 
 func DecodeString(s string) string {
-	var st []int32
-
+	stk := make([]int32, 0)
 	for _, v := range s {
 		if v == ']' {
-			subStr := make([]int32, 0)
-			for st[len(st)-1] != '[' {
-				subStr = append(subStr, st[len(st)-1])
-				st = st[:len(st)-1]
+			var subStr []int32
+			for len(stk) > 0 && stk[len(stk)-1] != '[' {
+				subStr = append(subStr, stk[len(stk)-1])
+				stk = stk[:len(stk)-1]
 			}
-			st = st[:len(st)-1]
-			k := 0
-			base := 1
-			for len(st) > 0 && st[len(st)-1] >= '0' && st[len(st)-1] <= '9' {
-				k += int(st[len(st)-1]-'0') * base
+			stk = stk[:len(stk)-1]
+			var k int32
+			var base int32
+			base = 1
+			for len(stk) > 0 && stk[len(stk)-1] >= '0' && stk[len(stk)-1] <= '9' {
+				k += base * (stk[len(stk)-1] - '0')
 				base *= 10
-				st = st[:len(st)-1]
+				stk = stk[:len(stk)-1]
 			}
 			for k > 0 {
 				for i := len(subStr) - 1; i >= 0; i-- {
-					st = append(st, subStr[i])
+					stk = append(stk, subStr[i])
 				}
 				k--
 			}
 		} else {
-			st = append(st, v)
+			stk = append(stk, v)
 		}
 	}
-
 	var result strings.Builder
-	for _, v := range st {
+	for _, v := range stk {
 		result.WriteRune(rune(v))
 	}
 	return result.String()
