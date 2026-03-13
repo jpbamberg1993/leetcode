b6635cc0d139f849a96474e05fb77b75b4218c8a
Paul Bamberg <jpbamberg1993@gmail.com>
2026-03-13 19:03:11 -0400

    649. Dota Senate - benchmark

--- go-solutions/649_dota_senate_test.go
+++ go-solutions/649_dota_senate_test.go
@@ -12,12 +12,21 @@ type dotaSenateTest struct {
 var dotaSenateTests = []dotaSenateTest{
 	{"RD", "Radiant"},
 	{"RDD", "Dire"},
+	{"RD", "Radiant"},
 }
 
 func Test_DotaSenate(t *testing.T) {
 	for _, test := range dotaSenateTests {
 		if output := predictPartyVictory(test.senate); output != test.expect {
 			t.Errorf("Input senate: %q => %q not equal to %q", test.senate, output, test.expect)
+		}
+	}
+}
+
+func Benchmark_DotaSenate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, t := range dotaSenateTests {
+			_ = predictPartyVictory(t.senate)
 		}
 	}
 }
