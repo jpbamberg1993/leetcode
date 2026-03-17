f02146f2ee0d73d0cd2ba3648d961945d8c59c87
Paul Bamberg <jpbamberg1993@gmail.com>
2026-03-17 18:07:42 -0400

    328. Odd even list - solve again using two pointers

--- go-solutions/328_odd_even_list_test.go
+++ go-solutions/328_odd_even_list_test.go
@@ -14,6 +14,7 @@ type oddEvenTest struct {
 var oddEvenTests = []oddEvenTest{
 	{head: []int{1, 2, 3, 4, 5}, expect: []int{1, 3, 5, 2, 4}},
 	{head: []int{2, 1, 3, 5, 6, 4, 7}, expect: []int{2, 3, 6, 7, 1, 5, 4}},
+	{head: []int{}, expect: []int{}},
 }
 
 func Test_OddEvenList(t *testing.T) {
