0fdf8e259e43f64d41a13ff6280d0fda3ab1b403
Paul Bamberg <jpbamberg1993@gmail.com>
2026-03-25 17:28:25 -0400

    2130. Pair Sum - solve again o(n)

--- go-solutions/2130_pair_sum.go
+++ go-solutions/2130_pair_sum.go
@@ -5,19 +5,19 @@ import (
 )
 
 func pairSum(head *utils.ListNode) int {
-	middle := getMiddle(head)
-	reverse := reverseList(middle)
-	maxSum := 0
-	for reverse != nil {
-		maxSum = max(maxSum, reverse.Val+head.Val)
+	middle := findMiddle(head)
+	reversed := reverseList(middle.Next)
+	maxPair := 0
+	for reversed != nil {
+		maxPair = max(reversed.Val+head.Val, maxPair)
+		reversed = reversed.Next
 		head = head.Next
-		reverse = reverse.Next
 	}
-	return maxSum
+	return maxPair
 }
 
-func getMiddle(head *utils.ListNode) *utils.ListNode {
-	slow, fast := head, head
+func findMiddle(head *utils.ListNode) *utils.ListNode {
+	slow, fast := head, head.Next
 	for fast != nil && fast.Next != nil {
 		slow = slow.Next
 		fast = fast.Next.Next
