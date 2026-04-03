a0540ad701bdb19065659ec24ce9d3af300875ef
Paul Bamberg <jpbamberg1993@gmail.com>
2026-04-03 09:14:38 -0400

    1372. Longest ZigZag - Solve again using dps

--- go-solutions/1372_longest_zig_zag.go
+++ go-solutions/1372_longest_zig_zag.go
@@ -2,24 +2,24 @@ package leetcode
 
 import "go-solutions/utils"
 
-var maxLength int
+var maxPathLen int
 
-func longestZigZag(input *utils.TreeNode) int {
-	maxLength = 0
-	dps(input, true, 0)
-	return maxLength
+func longestZigZag(node *utils.TreeNode) int {
+	maxPathLen = 0
+	dps(node, true, 0)
+	return maxPathLen
 }
 
-func dps(node *utils.TreeNode, goingLeft bool, runningLen int) {
+func dps(node *utils.TreeNode, goLeft bool, pathLen int) {
 	if node == nil {
 		return
 	}
-	maxLength = max(maxLength, runningLen)
-	if goingLeft {
-		dps(node.Left, false, runningLen+1)
+	maxPathLen = max(pathLen, maxPathLen)
+	if goLeft {
+		dps(node.Left, false, pathLen+1)
 		dps(node.Right, true, 1)
 	} else {
-		dps(node.Right, true, runningLen+1)
+		dps(node.Right, true, pathLen+1)
 		dps(node.Left, false, 1)
 	}
 }
