887621af995997886cd6f95bb5d761815f0a80be
Paul Bamberg <jpbamberg1993@gmail.com>
2026-04-10 09:33:07 -0400

    104. Max Depth BTree - Solve again using recursion

--- go-solutions/104_max_depth.go
+++ go-solutions/104_max_depth.go
@@ -6,9 +6,7 @@ func maxDepth(root *utils.TreeNode) int {
 	if root == nil {
 		return 0
 	}
-
-	left := maxDepth(root.Left)
-	right := maxDepth(root.Right)
-
-	return max(left, right) + 1
+	leftCount := maxDepth(root.Left)
+	rightCount := maxDepth(root.Right)
+	return max(leftCount, rightCount) + 1
 }
--- go-solutions/104_max_depth_test.go
+++ go-solutions/104_max_depth_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestMaxDepth_Example1(t *testing.T) {
+func TestMaxDepth(t *testing.T) {
 	t.Run("example1", func(t *testing.T) {
 		values := []any{3, 9, 20, nil, nil, 15, 7}
 		tree := utils.BuildTree(values)
@@ -15,12 +15,12 @@ func TestMaxDepth_Example1(t *testing.T) {
 		}
 	})
 
-	t.Run("example2", func(t *testing.T) {
-		values := []any{1, nil, 2}
-		tree := utils.BuildTree(values)
-		expect := 2
-		if result := maxDepth(tree); result != expect {
-			t.Errorf("received %d but expected %d", result, expect)
-		}
-	})
+	//t.Run("example2", func(t *testing.T) {
+	//	values := []any{1, nil, 2}
+	//	tree := utils.BuildTree(values)
+	//	expect := 2
+	//	if result := maxDepth(tree); result != expect {
+	//		t.Errorf("received %d but expected %d", result, expect)
+	//	}
+	//})
 }
