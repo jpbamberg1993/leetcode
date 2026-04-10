979700d719258fc42c4e5022aade507f9c2cc58c
Paul Bamberg <jpbamberg1993@gmail.com>
2026-04-10 09:07:44 -0400

    236. Lowest Common Ancestor - solve using recursion

--- go-solutions/236_lca.go
+++ go-solutions/236_lca.go
@@ -1,1 +1,34 @@
 package leetcode
+
+import "go-solutions/utils"
+
+var ans *utils.TreeNode
+
+func lowestCommonAncestor(root, p, q *utils.TreeNode) *utils.TreeNode {
+	ans = nil
+	scanTree(root, p, q)
+	return ans
+}
+
+func scanTree(node *utils.TreeNode, p *utils.TreeNode, q *utils.TreeNode) bool {
+	if node == nil {
+		return false
+	}
+	leftFound := scanTree(node.Left, p, q)
+	rightFound := scanTree(node.Right, p, q)
+	mid := node == p || node == q
+	count := 0
+	if leftFound {
+		count++
+	}
+	if rightFound {
+		count++
+	}
+	if mid {
+		count++
+	}
+	if count >= 2 {
+		ans = node
+	}
+	return count >= 1
+}
--- go-solutions/236_lca_test.go
+++ go-solutions/236_lca_test.go
@@ -1,1 +1,56 @@
 package leetcode
+
+import (
+	"go-solutions/utils"
+	"testing"
+)
+
+func Test_LCA(t *testing.T) {
+	t.Run("Test root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1 => 3", func(t *testing.T) {
+		root := []any{3, 5, 1, 6, 2, 0, 8, nil, nil, 7, 4}
+		rootNode := utils.BuildTree(root)
+		pNode := utils.DPS(rootNode, 5)
+		qNode := utils.DPS(rootNode, 1)
+		lowestCommonNode := lowestCommonAncestor(rootNode, pNode, qNode)
+		expectedNode := utils.DPS(rootNode, 3)
+		if lowestCommonNode != expectedNode {
+			if lowestCommonNode == nil {
+				t.Errorf("expected node %d, got nil", expectedNode.Val)
+			} else {
+				t.Errorf("expected node %d, got node %d", expectedNode.Val, lowestCommonNode.Val)
+			}
+		}
+	})
+
+	t.Run("Test root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4 => 5", func(t *testing.T) {
+		root := []any{3, 5, 1, 6, 2, 0, 8, nil, nil, 7, 4}
+		rootNode := utils.BuildTree(root)
+		pNode := utils.DPS(rootNode, 5)
+		qNode := utils.DPS(rootNode, 4)
+		lowestCommonNode := lowestCommonAncestor(rootNode, pNode, qNode)
+		expectedNode := utils.DPS(rootNode, 5)
+		if lowestCommonNode != expectedNode {
+			if lowestCommonNode == nil {
+				t.Errorf("expected node %d, got nil", expectedNode.Val)
+			} else {
+				t.Errorf("expected node %d, got node %d", expectedNode.Val, lowestCommonNode.Val)
+			}
+		}
+	})
+
+	t.Run("Test root = [1,2], p = 1, q = 2 => 1", func(t *testing.T) {
+		root := []any{1, 2}
+		rootNode := utils.BuildTree(root)
+		pNode := utils.DPS(rootNode, 1)
+		qNode := utils.DPS(rootNode, 2)
+		lowestCommonNode := lowestCommonAncestor(rootNode, pNode, qNode)
+		expectedNode := utils.DPS(rootNode, 1)
+		if lowestCommonNode != expectedNode {
+			if lowestCommonNode == nil {
+				t.Errorf("expected node %d, got nil", expectedNode.Val)
+			} else {
+				t.Errorf("expected node %d, got node %d", expectedNode.Val, lowestCommonNode.Val)
+			}
+		}
+	})
+}
