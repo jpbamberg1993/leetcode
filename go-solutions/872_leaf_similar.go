d10c7e7dc3f8068ce70a125792585333f04ff6cc
Paul Bamberg <jpbamberg1993@gmail.com>
2026-04-14 08:12:40 -0400

    872. Leaf Similar - solve again using recursion

--- go-solutions/872_leaf_similar.go
+++ go-solutions/872_leaf_similar.go
@@ -1,24 +1,22 @@
 package leetcode
 
-import (
-	"go-solutions/utils"
-	"reflect"
-)
+import "go-solutions/utils"
 
 func leafSimilar(root1, root2 *utils.TreeNode) bool {
-	var leaves1 []int
-	var leaves2 []int
-	getLeaves(root1, &leaves1)
-	getLeaves(root2, &leaves2)
-	return reflect.DeepEqual(leaves1, leaves2)
+	leaves1 := getLeaves(root1, []int{})
+	leaves2 := getLeaves(root2, []int{})
+	return utils.SliceEqual(leaves1, leaves2)
 }
 
-func getLeaves(node *utils.TreeNode, leaves *[]int) {
-	if node != nil {
-		if node.Left == nil && node.Right == nil {
-			*leaves = append(*leaves, node.Val)
-		}
-		getLeaves(node.Left, leaves)
-		getLeaves(node.Right, leaves)
+func getLeaves(node *utils.TreeNode, leaves []int) []int {
+	if node == nil {
+		return leaves
 	}
+	if node.Left == nil && node.Right == nil {
+		leaves = append(leaves, node.Val)
+		return leaves
+	}
+	leaves = getLeaves(node.Left, leaves)
+	leaves = getLeaves(node.Right, leaves)
+	return leaves
 }
